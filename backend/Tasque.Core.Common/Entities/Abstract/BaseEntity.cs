82ea2e402cd17afe4a7c4518f93afb260c3149e9
andpew <edarkmail@gmail.com>
2022-08-08 16:37:36 +0300

    Added abstract base entity

---
+++ backend/Tasque.Core.Common/Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Tasque.Core.Common.Entities.Abstract;
+
+public class BaseEntity
+{
+    public BaseEntity()
+    {
+        CreatedAt = UpdatedAt = DateTime.Now;
+    }
+
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
