3fcac271aa0b47c852e470db8304165bea367c6a
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-08 14:06:53 +0300

    configured appBuilder to use NLog

--- backend/Tasque.WebAPI/Program.cs
+++ backend/Tasque.WebAPI/Program.cs
@@ -1,7 +1,10 @@
+using NLog.Extensions.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Logging.AddNLog();
 
 var app = builder.Build();
 
