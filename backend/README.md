ec16af37f95b2cc05aaab2237e4f50ce5c48b497
Setup1337 <thevadimchanel@gmail.com>
2022-08-08 13:33:44 +0200

    Apply useMigrationEndPoint to all types of projects, added README.md and added whitespaces in the end of files

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+﻿## Make migration
+
+Use the dotnet cli to make migration.
+
+```bash
+dotnet ef migrations add MigrationName --startup-project ./ --project ..\Tasque.DAL\
+```
--- backend/Tasque.WebAPI/Program.cs
+++ backend/Tasque.WebAPI/Program.cs
@@ -19,10 +19,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-else
-{
-    app.UseMigrationsEndPoint();
-}
+
+
+app.UseMigrationsEndPoint();
 
 
 app.UseHttpsRedirection();
