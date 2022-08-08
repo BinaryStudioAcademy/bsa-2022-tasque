33d8b8dc798e7b3b22bd23cce40b9ed093b377ef
andpew <edarkmail@gmail.com>
2022-08-08 17:59:52 +0300

    Created dbsets from entities

--- backend/Tasque.DAL/DataContext.cs
+++ backend/Tasque.DAL/DataContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Tasque.Core.Common.Entities;
+using Task = Tasque.Core.Common.Entities.Task;
 
 namespace Tasque.Core.DAL;
 
@@ -11,13 +12,25 @@ public class DataContext : DbContext
 
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
-        
+
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseSerialColumns();
     }
 
+    public DbSet<Organization>? Organizations { get; set; }
     public DbSet<User>? Users { get; set; }
     public DbSet<Project>? Projects { get; set; }
+    public DbSet<Task>? Tasks { get; set; }
+    public DbSet<Comment>? Comments { get; set; }
+    public DbSet<Attachment>? Attachments { get; set; }
+    public DbSet<Board>? Boards { get; set; }
+    public DbSet<BoardColumn>? BoardColumns { get; set; }
+    public DbSet<Calendar>? Calendars { get; set; }
+    public DbSet<Label>? Labels { get; set; }
+    public DbSet<Meeting>? Meetings { get; set; }
+    public DbSet<Notification>? Notifications { get; set; }
+    public DbSet<Role>? Roles { get; set; }
+    public DbSet<Sprint>? Sprints { get; set; }
 }
