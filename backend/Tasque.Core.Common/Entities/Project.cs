244ce8a71660762d2aba7308eece38765874d473
andpew <edarkmail@gmail.com>
2022-08-08 17:49:02 +0300

    Updated entities by adding many-to-many rels And links on other entities

--- backend/Tasque.Core.Common/Entities/Project.cs
+++ backend/Tasque.Core.Common/Entities/Project.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Tasque.Core.Common.Entities.Abstract;
+﻿using Tasque.Core.Common.Entities.Abstract;
 
 namespace Tasque.Core.Common.Entities;
 
 public class Project : BaseEntity
 {
-    [Required]
     public string Name { get; set; } = "";
+
     public int AuthorId { get; set; }
+    public User Author { get; set; }
+
     public int OrganizationId { get; set; }
+    public Organization Organization { get; set; }
+
+    public virtual ICollection<User> Users { get; set; }
 }
--- backend/Tasque.Core.Common/Entities/User.cs
+++ backend/Tasque.Core.Common/Entities/User.cs
@@ -1,13 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Tasque.Core.Common.Entities.Abstract;
+﻿using Tasque.Core.Common.Entities.Abstract;
 
 namespace Tasque.Core.Common.Entities;
 
 public class User : BaseEntity
 {
-    [Required]
     public string Name { get; set; } = "";
     public string Email { get; set; } = "";
     public string Password { get; set; } = "";
     public string Salt { get; set; } = "";
+
+    public virtual ICollection<Task> Tasks { get; set; }
+    public virtual ICollection<Meeting> Meetings { get; set; }
+    public virtual ICollection<Project> Projects { get; set; }
+    public virtual ICollection<Role> Roles { get; set; }
 }
