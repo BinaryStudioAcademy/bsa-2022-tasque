9a1b47cee811a4108cb577bed25805e8ffa2d949
Setup1337 <thevadimchanel@gmail.com>
2022-08-08 14:56:28 +0200

    All warnings and errors fixed

--- frontend/src/core/services/http.service.spec.ts
+++ frontend/src/core/services/http.service.spec.ts
@@ -3,7 +3,6 @@ import { HttpService } from './http.service';
 describe('HttpService', () => {
   let service: HttpService;
 
-
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
--- frontend/src/core/services/http.service.ts
+++ frontend/src/core/services/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpResponse, HttpHeaders, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
