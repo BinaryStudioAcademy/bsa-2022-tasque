3679d0d84d1727cb5f335fcec2d30759751871b2
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-08 15:58:25 +0300

    created by btn-link

--- frontend/src/app/component/button/button.component.ts
+++ frontend/src/app/component/button/button.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.sass']
+	selector: 'app-button',
+	templateUrl: './button.component.html',
+	styleUrls: ['./button.component.sass']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
+  public buttonText = '';
+  public buttonClass = 'btn';
 
-  constructor() { }
+	@Input()
+	set text(name: string) {
+		this.buttonText = name;
+	}
+	get name(): string {
+		return this.buttonText;
+	}
 
-  ngOnInit(): void {
-  }
+  @Input()
+	set class(name: string) {
+		this.buttonClass = name;
+	}
+	get class(): string {
+		return this.buttonClass;
+	}
 
+	@Input() color: string = '0068B4';
+	@Input() type: string = 'button';
+	@Output() btnClick = new EventEmitter();
+
+	constructor() {}
+
+	onClick() {
+		this.btnClick.emit();
+	}
 }
