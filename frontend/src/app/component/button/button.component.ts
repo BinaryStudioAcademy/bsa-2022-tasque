33aee7c9cf9ea20b9742222a24c71ac44365ed92
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-08 16:52:14 +0300

    bug fix

--- frontend/src/app/component/button/button.component.ts
+++ frontend/src/app/component/button/button.component.ts
@@ -1,37 +1,36 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-	selector: 'app-button',
-	templateUrl: './button.component.html',
-	styleUrls: ['./button.component.sass']
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.sass'],
 })
 export class ButtonComponent {
   public buttonText = '';
   public buttonClass = 'btn';
 
-	@Input()
-	set text(name: string) {
-		this.buttonText = name;
-	}
-	get name(): string {
-		return this.buttonText;
-	}
+  @Input()
+  set text(name: string) {
+    this.buttonText = name;
+  }
+  get name(): string {
+    return this.buttonText;
+  }
 
   @Input()
-	set class(name: string) {
-		this.buttonClass = name;
-	}
-	get class(): string {
-		return this.buttonClass;
-	}
+  set class(name: string) {
+    this.buttonClass = name;
+  }
+  get class(): string {
+    return this.buttonClass;
+  }
 
-	@Input() color: string = '0068B4';
-	@Input() type: string = 'button';
-	@Output() btnClick = new EventEmitter();
+  
+  @Output() btnClick = new EventEmitter();
 
-	constructor() {}
+  constructor() {}
 
-	onClick() {
-		this.btnClick.emit();
-	}
+  onClick(): void {
+    this.btnClick.emit();
+  }
 }
