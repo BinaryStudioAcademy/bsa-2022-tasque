be30e0e5010eae64da2be6769d6a68d3ce823f7c
Setup1337 <thevadimchanel@gmail.com>
2022-08-08 15:10:00 +0200

    Removed http.service.spec.ts

--- frontend/src/core/services/http.service.spec.ts
+++
@@ -1,9 +0,0 @@
-import { HttpService } from './http.service';
-
-describe('HttpService', () => {
-  let service: HttpService;
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
