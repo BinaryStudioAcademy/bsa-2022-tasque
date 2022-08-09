2837387bb7e6be1e5a08879bfa0e72015cfdee35
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-09 15:07:30 +0300

    fixed bugs

--- frontend/src/core/services/notification.service.ts
+++ frontend/src/core/services/notification.service.ts
@@ -2,34 +2,34 @@ import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({ providedIn: 'root' })
-export class NotificationServices {
+export class NotificationService {
   public constructor(private toastr: ToastrService) {}
 
   position = 'toast-bottom-center';
   timeOut = 3000;
 
-  public Error(msg: string, title = ''): void {
+  public error(msg: string, title = ''): void {
     this.toastr.error(msg, title, {
       timeOut: this.timeOut,
       positionClass: this.position,
     });
   }
 
-  public Success(msg: string, title = ''): void {
+  public success(msg: string, title = ''): void {
     this.toastr.success(msg, title, {
       timeOut: this.timeOut,
       positionClass: this.position,
     });
   }
 
-  public Info(msg: string, title = ''): void {
+  public info(msg: string, title = ''): void {
     this.toastr.info(msg, title, {
       timeOut: this.timeOut,
       positionClass: this.position,
     });
   }
 
-  public Warning(msg: string, title = ''): void {
+  public warning(msg: string, title = ''): void {
     this.toastr.warning(msg, title, {
       timeOut: this.timeOut,
       positionClass: this.position,
