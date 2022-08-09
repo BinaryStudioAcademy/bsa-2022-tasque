71f87f03867791e0bb59d051e16fb9894c91f5ae
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-09 14:46:21 +0300

    renamed DTOs

--- backend/Tasque.Core.Common/DTO/UserLoginDto.cs
+++ backend/Tasque.Core.Common/DTO/UserLoginDto.cs
@@ -1,9 +1,8 @@
 ﻿namespace Tasque.Core.Common.DTO
 {
-    public class RegisterDto
+    public class UserLoginDto
     {
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
-        public string Name { get; set; } = null!;
     }
 }
--- backend/Tasque.Core.Common/DTO/UserRegisterDto.cs
+++ backend/Tasque.Core.Common/DTO/UserRegisterDto.cs
@@ -1,8 +1,9 @@
 ﻿namespace Tasque.Core.Common.DTO
 {
-    public class LoginDto
+    public class UserRegisterDto
     {
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
+        public string Name { get; set; } = null!;
     }
 }
