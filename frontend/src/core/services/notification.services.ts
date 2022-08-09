6314cd2cce8c900498f6f9e40b7dc8616266d8b5
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-09 13:59:01 +0300

    added method types

--- frontend/src/core/services/notification.services.ts
+++ frontend/src/core/services/notification.services.ts
@@ -5,30 +5,38 @@ import { ToastrService } from 'ngx-toastr';
 export class NotificationServices {
   public constructor(private toastr: ToastrService) {}
 
-  position : string = 'toast-bottom-center';
+  position: string = 'toast-bottom-center';
 
-  public Error(msg: string, title: string = '', timeOut: number = 3000) {
+  public Error(msg: string, title: string = '', timeOut: number = 3000): void {
     this.toastr.error(msg, title, {
       timeOut: timeOut,
       positionClass: this.position,
     });
   }
 
-  public Success(msg: string, title: string = '', timeOut: number = 3000) {
+  public Success(
+    msg: string,
+    title: string = '',
+    timeOut: number = 3000,
+  ): void {
     this.toastr.success(msg, title, {
       timeOut: timeOut,
       positionClass: this.position,
     });
   }
 
-  public Info(msg: string, title: string = '', timeOut: number = 3000) {
+  public Info(msg: string, title: string = '', timeOut: number = 3000): void {
     this.toastr.info(msg, title, {
       timeOut: timeOut,
       positionClass: this.position,
     });
   }
 
-  public Warning(msg: string, title: string = '', timeOut: number = 3000) {
+  public Warning(
+    msg: string,
+    title: string = '',
+    timeOut: number = 3000,
+  ): void {
     this.toastr.warning(msg, title, {
       timeOut: timeOut,
       positionClass: this.position,
