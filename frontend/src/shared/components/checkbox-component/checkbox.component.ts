08f45f79c5a05bed6b56ce3a660bb2fd4f8c4a69
andpew <edarkmail@gmail.com>
2022-08-09 10:06:11 +0300

    Created checkbox component

---
+++ frontend/src/shared/components/checkbox-component/checkbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox',
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.sass']
+})
+export class CheckboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
