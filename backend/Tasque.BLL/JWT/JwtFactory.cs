8edc0ded7236c3f56c6e8c76a12c005e1c6bb61c
Daniil <daniilpoyarkov@gmail.com>
2022-08-09 11:08:37 +0300

    Removed comments

--- backend/Tasque.BLL/JWT/JwtFactory.cs
+++ backend/Tasque.BLL/JWT/JwtFactory.cs
@@ -19,13 +19,13 @@ namespace Tasque.Core.BLL.JWT
             _config = config;
         }
 
-        public string GenerateToken(object user) //Replace object to UserModel (not UserLogin_model)
+        public string GenerateToken(object user)
         {
             var jwtOptions = _config.GetSection("JwtIssuerOptions");
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("JWTIssuerOptions")["Key"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-            var claims = new Claim[] { };//here should be user data, except login and password, such as Name, email ect.
+            var claims = new Claim[] { };
 
             var token = new JwtSecurityToken(jwtOptions["Issuer"],
                 jwtOptions["Audience"],
@@ -38,10 +38,7 @@ namespace Tasque.Core.BLL.JWT
 
         public object Authenticate(object credentials)
         {
-            //// Here you should place linq request to get user entity from DbContext using User Login and Password
-            //    if (entity != null) //Use this method to return User entity. Before this <<if>> place linq to get entity from DBContext by login and password
-            //        return entity; //Also replace object which shoud get in this method with UserLoginModel, which has user Login and Password
-            return null; ////If user Entity not found it will return null, so you can throw excepption here(for example you can create custom NotFFoundException and throw it), by the way GlobalErrorExceptionHandler should handle this exception.
+            throw new NotImplementedException();
         }
     }
 }
