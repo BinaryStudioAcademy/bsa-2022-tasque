9d626a145064ae2685a0a09e7306a33dfb8ed447
Its_OP <prostakov.oleh@gmail.com>
2022-08-09 20:23:19 +0300

    CRUD service interface was added

---
+++ backend/Tasque.BLL/Services/IEntityCrudService.cs
@@ -0,0 +1,13 @@
+﻿using Tasque.Core.Common.Entities.Abstract;
+
+namespace Tasque.Core.BLL.Services
+{
+    public interface IEntityCrudService<TEntity> where TEntity : BaseEntity
+    {
+        TEntity Create(TEntity entity);
+        bool Delete(int id);
+        IEnumerable<TEntity> ReadAll();
+        TEntity? ReadOne(int id);
+        TEntity Update(TEntity entity);
+    }
+}
