699bf3c5b165e04fae6961591367104aa4a2102b
Daniil <daniilpoyarkov@gmail.com>
2022-08-09 11:03:42 +0300

    replaced JWT logic to Tasque.Core.BLL.JWT folder

--- backend/Tasque.BLL/JWT/JwtFactory.cs
+++ backend/Tasque.BLL/JWT/JwtFactory.cs
@@ -1,39 +1,25 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Tasque.Core.WebAPI.Controllers
+namespace Tasque.Core.BLL.JWT
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TokenController : ControllerBase
+    public class JwtFactory
     {
-        private readonly IConfiguration _config;
+        public readonly IConfiguration _config;
 
-        public TokenController(IConfiguration config)
+        public JwtFactory(IConfiguration config)
         {
             _config = config;
         }
 
-        [AllowAnonymous]
-        [HttpPost]
-        public IActionResult Login([FromBody] object credentials) //Replace <<object>> with UserLoginModel model, for example UserLogin, which has User login and password. Not More
-        {
-            var user = Authenticate(credentials);
-
-            if(user != null)
-            {
-                var token = GenerateToken(user);
-                return Ok(token);
-            }
-            return NotFound("Not found");
-        }
-
-        private string GenerateToken(object user) //Replace object to UserModel (not UserLogin_model)
+        public string GenerateToken(object user) //Replace object to UserModel (not UserLogin_model)
         {
             var jwtOptions = _config.GetSection("JwtIssuerOptions");
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("JWTIssuerOptions")["Key"]));
@@ -50,12 +36,12 @@ namespace Tasque.Core.WebAPI.Controllers
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
-        private object Authenticate(object credentials)
+        public object Authenticate(object credentials)
         {
             //// Here you should place linq request to get user entity from DbContext using User Login and Password
             //    if (entity != null) //Use this method to return User entity. Before this <<if>> place linq to get entity from DBContext by login and password
             //        return entity; //Also replace object which shoud get in this method with UserLoginModel, which has user Login and Password
-                return null; ////If user Entity not found it will return null, so you can throw excepption here(for example you can create custom NotFFoundException and throw it), by the way GlobalErrorExceptionHandler should handle this exception.
+            return null; ////If user Entity not found it will return null, so you can throw excepption here(for example you can create custom NotFFoundException and throw it), by the way GlobalErrorExceptionHandler should handle this exception.
         }
     }
 }
