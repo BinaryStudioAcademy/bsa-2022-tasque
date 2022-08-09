7433c7d0efaa0970c1abf353d70e74d1ccb6dd72
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-09 14:57:26 +0300

    added register method in service

--- backend/Tasque.BLL/Services/AuthService.cs
+++ backend/Tasque.BLL/Services/AuthService.cs
@@ -1,17 +1,39 @@
-﻿using Tasque.Core.Common.DTO;
+﻿using AutoMapper;
+using Tasque.Core.Common.DTO;
+using Tasque.Core.Common.Entities;
+using Tasque.Core.Common.Security;
+using Tasque.Core.DAL;
 
 namespace Tasque.Core.BLL.Services
 {
     public class AuthService
     {
-        public Task<string> Login(LoginDto loginInfo)
+        private DataContext _context;
+        private IMapper _mapper;
+
+        public AuthService(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<string> Login(UserLoginDto loginInfo)
         {
             throw new NotImplementedException();
         }
 
-        public Task<string> Register(RegisterDto registerInfo)
+        public async Task<UserDto> Register(UserRegisterDto registerInfo)
         {
-            throw new NotImplementedException();
+            var userEntity = _mapper.Map<User>(registerInfo);
+            var salt = SecurityHelper.GetRandomBytes();
+
+            userEntity.Salt = Convert.ToBase64String(salt);
+            userEntity.Password = SecurityHelper.HashPassword(registerInfo.Password, salt);
+
+            _context.Users.Add(userEntity);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<UserDto>(userEntity);
         }
     }
 }
