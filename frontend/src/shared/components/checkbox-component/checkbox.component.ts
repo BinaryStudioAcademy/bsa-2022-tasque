b14eb3260a534c3faa32abbb444d3166599b8c91
andpew <edarkmail@gmail.com>
2022-08-09 13:54:14 +0300

    Updated custom checkbox

--- frontend/src/shared/components/checkbox-component/checkbox.component.ts
+++ frontend/src/shared/components/checkbox-component/checkbox.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.sass']
 })
 export class CheckboxComponent implements OnInit {
+  @Input() public Text = '';
+  @Input() public Checked = false;
+  @Output() public stateChanged = new EventEmitter<boolean>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public toggleChecked() {
+    this.Checked = !this.Checked;
+    this.stateChanged.emit(this.Checked);
+  }
 }
