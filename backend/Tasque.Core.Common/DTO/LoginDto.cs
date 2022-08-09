2329f9be6ca884437c5d44245265286502fbb047
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-09 10:22:58 +0300

    added DTOs and controller template

---
+++ backend/Tasque.Core.Common/DTO/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Tasque.Core.Common.DTO
+{
+    public class LoginDto
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ backend/Tasque.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tasque.Core.Common.DTO;
+
+namespace Tasque.Core.WebAPI.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public Task<IActionResult> Login([FromBody] LoginDto loginInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Register([FromBody] RegisterDto registerInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
