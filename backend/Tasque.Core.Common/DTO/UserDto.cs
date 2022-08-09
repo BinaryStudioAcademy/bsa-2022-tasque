147d9d9764996457f053f7d1cfaae7df32b10aaa
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-09 14:52:27 +0300

    added userDto

---
+++ backend/Tasque.Core.Common/DTO/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Tasque.Core.Common.DTO
+{
+    public class UserDto
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
