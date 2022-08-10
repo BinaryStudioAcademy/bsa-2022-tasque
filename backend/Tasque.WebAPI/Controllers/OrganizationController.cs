4dd145d22815004c3fc1680ba19fd638272c8e71
Daniil <daniilpoyarkov@gmail.com>
2022-08-10 13:20:40 +0300

    deleted comments and changed colors with color-scheme

--- backend/Tasque.WebAPI/Controllers/OrganizationController.cs
+++ backend/Tasque.WebAPI/Controllers/OrganizationController.cs
@@ -2,27 +2,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Tasque.Core.Common.Entities;
 using Tasque.Core.Common.IncomeModels;
-//using Tasque.Core.BLL.Services;
 
 namespace Tasque.Core.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class OrganizationController : ControllerBase
     {
-        //private readonly EntityCrudService<Organization> _organizationService;
-
-        //public OrganizationController(EntityCrudService<Organization> organizationService)
-        //{
-        //    _organizationService = organizationService;
-        //}
 
         [HttpPost("createOrganization")]
         public IActionResult CreateOrganization([FromBody] CreateOrganization organization)
         {
-            //var entity = _organizationService.Create(organization);
-            return Created(organization.ToString()?? "", organization);
+            throw new NotImplementedException("Waiting for generic CRUD");
         }
     }
 }
