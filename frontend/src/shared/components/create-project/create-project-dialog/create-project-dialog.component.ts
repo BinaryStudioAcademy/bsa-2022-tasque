3d661919f88363003643dbf4868b72f416ac4521
Setup1337 <thevadimchanel@gmail.com>
2022-08-10 19:58:55 +0200

    Created "create project" modal dialog

---
+++ frontend/src/shared/components/create-project/create-project-dialog/create-project-dialog.component.ts
@@ -0,0 +1,61 @@
+﻿import { Component, Inject, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { ProjectService } from '../../../../core/services/project.service';
+import { NewProjectModel } from '../../../../core/models/project/new-project-model';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UserModel } from '../../../../core/models/user/user-model';
+import { OrganizationModel } from '../../../../core/models/organization/organization-model';
+import { takeUntil } from 'rxjs/operators';
+import {NewProjectCredentialsModel} from "../../../../core/models/project/new-project-credentials.model";
+
+@Component({
+  selector: 'app-create-project-dialog',
+  templateUrl: './create-project-dialog.component.html',
+  styleUrls: ['./create-project-dialog.component.sass']
+})
+export class CreateProjectDialogComponent implements OnInit {
+
+  public createBtnName = 'Create project';
+  public createBtnClass = 'fill';
+
+  public cancelBtnName = 'Cancel';
+  public cancelBtnClass = 'fill gray';
+
+  public inputNameClass = 'input';
+  public inputNameType = 'text';
+  public inputNameId = 'projectName';
+  public inputNamePlaceholder = 'Try a team name, project goal, milestone...';
+  public inputNameLabel = 'Name';
+  public inputNameRequired = true;
+
+  public inputKeyClass = 'input';
+  public inputKeyType = 'text';
+  public inputKeyId = 'projectKey';
+  public inputKeyLabel = 'Key';
+
+  public newProjectName = '';
+
+  public unsubscribe$ = new Subject<void>();
+
+  constructor(
+    public projectService: ProjectService,
+    @Inject(MAT_DIALOG_DATA) public data: NewProjectCredentialsModel,
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(): void{
+
+    const newProject: NewProjectModel = {
+      name: this.newProjectName,
+      authorId: this.data.authorId,
+      organizationId: this.data.organizationId,
+    };
+    this.projectService.createProject(newProject)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe();
+  }
+
+}
---
+++ frontend/src/shared/components/create-project/create-project.component.ts
@@ -0,0 +1,48 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { CreateProjectDialogComponent } from './create-project-dialog/create-project-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { UserModel } from '../../../core/models/user/user-model';
+import { OrganizationModel } from '../../../core/models/organization/organization-model';
+import {NewProjectCredentialsModel} from "../../../core/models/project/new-project-credentials.model";
+
+@Component({
+  selector: 'app-create-project',
+  templateUrl: './create-project.component.html',
+  styleUrls: ['./create-project.component.sass']
+})
+export class CreateProjectComponent implements OnInit {
+
+  public currentUser: UserModel = {
+    name: 'Login1',
+    email: 'testlogin@gmail.com',
+    id: 1,
+    salt: 'Salt',
+    password: 'Password'
+  };
+  public currentOrganization: OrganizationModel = {
+    id: 1,
+    name: 'Organization1',
+    authorId: 1,
+    createdAt: new Date(2000,1,1),
+    updatedAt: new Date(2001,1,1),
+  };
+
+  public btnText = 'Create organization';
+  public btnClass = 'mini';
+  constructor(
+    public matDialog: MatDialog
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  openDialog():void {
+    const newProjectCredential: NewProjectCredentialsModel = {
+      authorId : this.currentUser.id,
+      organizationId: this.currentOrganization.id,
+    };
+    const dialog = this.matDialog.open(CreateProjectDialogComponent, { data: newProjectCredential });
+    dialog.afterClosed().subscribe();
+  }
+
+}
