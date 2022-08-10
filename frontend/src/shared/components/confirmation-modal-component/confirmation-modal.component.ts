e3110493de3612311bc550b70a7b825a78f9e14a
andpew <edarkmail@gmail.com>
2022-08-10 11:40:56 +0300

    Finally updated confirmation modal component

--- frontend/src/shared/components/confirmation-modal-component/confirmation-modal.component.ts
+++ frontend/src/shared/components/confirmation-modal-component/confirmation-modal.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MatDialog, MatDialogConfig, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export interface ConfirmationData {
+  title: string;
+  message: string;
+  type?: 'deletion' | 'default';
+}
 
 @Component({
   selector: 'app-confirmation-modal',
@@ -6,10 +14,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirmation-modal.component.sass']
 })
 export class ConfirmationModalComponent implements OnInit {
+  public type: string = 'default';
+  public message: string;
+  public title: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ConfirmationData) {
+    if (data.type) {
+      this.type = data.type;
+    }
+    this.message = data.message;
+    this.title = data.title;
   }
 
+  public onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  public onDismiss(): void {
+    this.dialogRef.close(false);
+  }
+
+  ngOnInit(): void { }
 }
