1b600dfaebebb36ba52d4be1daf60000167d190c
Daniil <daniilpoyarkov@gmail.com>
2022-08-10 20:43:50 +0300

    Added login, register and token models

--- frontend/src/core/services/auth.service.ts
+++ frontend/src/core/services/auth.service.ts
@@ -1,6 +1,9 @@
 import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AccessToken } from 'src/entity-models/access-token';
+import { UserLoginModel } from 'src/entity-models/user-login-model';
+import { UserRegisterModel } from 'src/entity-models/user-register-model';
 import { HttpService } from './http.service';
 
 @Injectable({
@@ -14,11 +17,11 @@ export class AuthService {
     private httpService: HttpService
   ) { }
 
-  loginUser(email: string, password: string):Observable<HttpResponse<{ assessToken: string }>> {
-    return this.httpService.postFullRequest(this.routePrefix + '/login', { email: email, password: password });
+  loginUser(credentials: UserLoginModel):Observable<HttpResponse<AccessToken>> {
+    return this.httpService.postFullRequest(this.routePrefix + '/login', credentials);
   }
 
-  registerUser(email: string, password: string, name: string):Observable<HttpResponse<{ assessToken: string }>> {
-    return this.httpService.postFullRequest(this.routePrefix + '/register', { email: email, password: password, name: name });
+  registerUser(credentials: UserRegisterModel):Observable<HttpResponse<AccessToken>> {
+    return this.httpService.postFullRequest(this.routePrefix + '/register', credentials);
   }
 }
