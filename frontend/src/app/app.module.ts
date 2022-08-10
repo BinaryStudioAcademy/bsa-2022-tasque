ce9506aac22b960f2ec5aa4f4401a0dc14c7a1f1
Daniil <101814817+DaniilPoiarkov@users.noreply.github.com>
2022-08-10 20:05:14 +0300

    Create create organization functionality (#23)
    * Fixed JwtKey configuration
    
    * Added create organization dialog button
    
    * Added models of organization and user
    
    * Added organization service to make requests
    
    * Changes according to linter errors
    
    * Implemented create organization functionality with MatDialog. Backend based on Generic Crud Service task
    
    * Fixed linter issues
    
    * Restored ToastrModule
    
    * Added apiUrl to env-prod.ts coz linterTest builing application and env files replaces
    
    * Changes acccording to comments
    
    * Fixes according to changes in dev branch
    
    * Deleted cache
    
    * Deleted empty lines
    
    * Fixed conflicts of module declarations
    
    * Removed useless imports
    
    * Changed PascalCase in interface to camelCase
    
    * Changes according to comments
    
    * Renamed folder
    
    * Removed useless attribute
    
    * deleted comments and changed colors with color-scheme
    
    * Changed color with colorschema
    
    * removed unneccessary attributes
    
    * Rename folder
    
    * Renamed namespace
    
    * Removed dublicats and addeduseage of input-component
    
    * Colorscheme added
    
    * removed useless styles
    
    * Added consistent spacing
    
    * Fix input attributes
    
    * removed multi empty-lines

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,20 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from 'src/core/core.module';
 import { SharedModule } from 'src/shared/shared.module';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { ToastrModule } from 'ngx-toastr';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+  ],
   imports: [
     BrowserModule,
     SharedModule,
     AppRoutingModule,
     CoreModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
     ToastrModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    BrowserAnimationsModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
