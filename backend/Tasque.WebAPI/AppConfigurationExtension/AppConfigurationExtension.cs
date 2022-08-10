f0d66d6edf80243af2d2cb46cd452b65e77e3b9f
Maxim <62253266+FeloniukM@users.noreply.github.com>
2022-08-10 16:33:01 +0300

    Add swagger (#27)
    add swagger

--- backend/Tasque.WebAPI/AppConfigurationExtension/AppConfigurationExtension.cs
+++ backend/Tasque.WebAPI/AppConfigurationExtension/AppConfigurationExtension.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 using System.Text;
 using Tasque.Core.BLL.JWT;
@@ -74,6 +75,41 @@ namespace Tasque.Core.WebAPI.AppConfigurationExtension
             services.AddControllers();
 
             services.AddScoped<AuthService>();
+        }
+
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then " +
+                    "your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+
+                    }
+                });
+
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tasque.Core.WebApi", Version = "v1" });
+            });
         }
     }
 }
