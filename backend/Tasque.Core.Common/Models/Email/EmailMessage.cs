d524c69cd4a80dda3e73ffa901659fef41694a4b
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-10 14:11:54 +0300

    added base email structure

---
+++ backend/Tasque.Core.Common/Models/Email/EmailMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace Tasque.Core.Common.Models.Email
+{
+    public class EmailMessage
+    {
+        public EmailContact Sender { get; set; }
+        public EmailContact Reciever { get; set; }
+        public string Subject { get; set; } = "";
+        public string Content { get; set; } = "";
+
+        public EmailMessage(EmailContact sender, EmailContact reciever)
+        {
+            Sender = sender;
+            Reciever = reciever;
+        }
+    }
+}
