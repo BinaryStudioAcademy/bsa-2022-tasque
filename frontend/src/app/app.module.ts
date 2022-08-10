01724ced91869679a195a8ed83ab4048f0578299
Daniil <daniilpoyarkov@gmail.com>
2022-08-10 12:01:50 +0300

    Removed useless imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,8 @@ import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateOrganizationComponent } from 'src/shared/components/create-organization/create-organization.component';
-import { CreateOrganizationDialogComponent } from '../shared/components/create-organization/create-organization-dialog/create-organization-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonComponent } from 'src/shared/components/tasque-button/button.component';
 
 @NgModule({
   declarations: [
