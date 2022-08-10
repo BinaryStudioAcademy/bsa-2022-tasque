a4de53ddad7dac3454abfd038164822638d81e02
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-10 18:55:50 +0300

    reimplemented exception handling mechanism

--- backend/Tasque.WebAPI/Middlewares/ExceptionMiddleware.cs
+++ backend/Tasque.WebAPI/Middlewares/ExceptionMiddleware.cs
@@ -21,25 +21,21 @@ namespace Tasque.Core.WebAPI.Middlewares
             try
             {
                 await _next(httpContext);
-            }
-            catch (HttpException ex)
+            }            
+            catch (Exception exception)
             {
-                _logger.LogError("{ex.StatusCode}| {ex.Message}", ex.StatusCode, ex.Message);
-                await CreateExceptionAsync(httpContext, ex.StatusCode, new { error = ex.Message });
-                return;
-            }
-            catch (ValidationException ex)
-            {
-                var message = ex.Errors.Any()
-                    ? ex.Errors.Aggregate("", (x, y) => x += $"{y.ErrorMessage}\n")
-                    : ex.Message;
-                await CreateExceptionAsync(httpContext, HttpStatusCode.BadRequest, message);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError("{ex.Message}", ex.Message);
-                await CreateExceptionAsync(httpContext);
-                return;
+                switch (exception)
+                {
+                    case HttpException ex:
+                        await HandleHttpException(httpContext, ex);
+                        break;
+                    case ValidationException ex:
+                        await HandleValidationException(httpContext, ex);
+                        break;
+                    default:
+                        await HandleGenericException(httpContext, exception);
+                        break;
+                }
             }
         }
         private async Task CreateExceptionAsync(HttpContext context, 
@@ -50,6 +46,25 @@ namespace Tasque.Core.WebAPI.Middlewares
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = (int)statusCode;
             await context.Response.WriteAsync(JsonConvert.SerializeObject(errorBody));
+        }
+
+        private async Task HandleGenericException(HttpContext context, Exception ex)
+        {
+            _logger.LogError("{ex.Message}", ex.Message);
+            await CreateExceptionAsync(context);
+        }
+        private async Task HandleHttpException(HttpContext context, HttpException ex)
+        {
+            _logger.LogError("{ex.StatusCode}| {ex.Message}", ex.StatusCode, ex.Message);
+            await CreateExceptionAsync(context, ex.StatusCode, new { error = ex.Message });
+            return;
+        }
+        private async Task HandleValidationException(HttpContext context, ValidationException ex)
+        {
+            var message = ex.Errors.Any()
+                    ? ex.Errors.Aggregate("", (x, y) => x += $"{y.ErrorMessage}\n")
+                    : ex.Message;
+            await CreateExceptionAsync(context, HttpStatusCode.BadRequest, message);
         }
     }
 }
