3795bf0fbbd8d606bc817b32d4ef77189ca49d21
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-10 16:23:07 +0300

    fixed nullref

--- backend/Tasque.Core.Common/Models/Email/EmailExtensions.cs
+++ backend/Tasque.Core.Common/Models/Email/EmailExtensions.cs
@@ -7,7 +7,7 @@ namespace Tasque.Core.Common.Models.Email
         public static TransactionalEmail ConvertForMailJet(this EmailMessage message)
         {
             return new TransactionalEmailBuilder()
-                .WithFrom(new SendContact(message.Sender.Email, message.Sender.Name))
+                .WithFrom(new SendContact(message.Sender?.Email, message.Sender?.Name))
                 .WithTo(new SendContact(message.Reciever.Email, message.Reciever.Name))
                 .WithSubject(message.Subject)
                 .WithHtmlPart(message.Content)
