3deac9697b9121a113014d1e2fad0d324d952c7c
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-11 11:39:01 +0300

    login page opens as soon as app starts (will be reverted in last commit)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/shared/services/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tasque';
+
+  constructor(private service: LoginService) {
+
+  }
+  ngOnInit(): void {
+    this.service.openLoginDialog();
+  }
 }
