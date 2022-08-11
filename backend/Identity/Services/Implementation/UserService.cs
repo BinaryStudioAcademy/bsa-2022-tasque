aa254b862d0acaa7649de3e429aa43de736a4132
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-08-11 20:58:22 +0300

    Users infrastructure was added

---
+++ backend/Identity/Services/Implementation/UserService.cs
@@ -0,0 +1,22 @@
+﻿using Tasque.Core.Identity.Services.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasque.Core.BLL.Services;
+using Tasque.Core.Common.Entities;
+using Tasque.Core.DAL;
+
+namespace Tasque.Core.Identity.Services.Implementation
+{
+    public class UserService : EntityCrudService<User>, IUserService
+    {
+        public UserService(DataContext db) : base(db) { }
+
+        public User? GetByEmail(string email)
+        {
+            return _db.Users.FirstOrDefault(u => u.Email == email);
+        }
+    }
+}
