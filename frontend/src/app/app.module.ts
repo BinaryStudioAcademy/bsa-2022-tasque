4303eb024c30a63c9c9e236824ebf6057ff0c148
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-11 18:55:21 +0300

    removed empty line

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent,],
-
   imports: [
     BrowserModule,
     SharedModule,
