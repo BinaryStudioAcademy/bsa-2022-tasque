ff6f5862b3109b317cb1cff2a9e456222766fef0
andpew <edarkmail@gmail.com>
2022-08-11 14:30:59 +0300

    Used ogr service for loading org if none is exists

--- frontend/src/shared/components/organizations-dropdown/organizations-dropdown.component.ts
+++ frontend/src/shared/components/organizations-dropdown/organizations-dropdown.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { OrganizationService } from 'src/core/services/organization.service';
 import { StorageService } from 'src/core/services/storage.service';
 import { OrganizationModel } from 'src/entity-models/organization-model';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'tasque-organizations-dropdown',
@@ -14,22 +17,50 @@ export class OrganizationsDropdownComponent implements OnInit {
     authorId: -1
   };
 
-  constructor(private storageService: StorageService) {
-    // make request for current organization
-  }
+  public unsubscribe$ = new Subject<void>();
+
+  constructor(
+    private organizationService: OrganizationService,
+    private storageService: StorageService) { }
 
   ngOnInit(): void {
     this.storageService.currentOrganizationId$.subscribe(
       (result) => {
-        const searchedOrganization = this.availableOrganizations.find((x) => x.id == result);
+        const searchedOrganization = this.availableOrganizations.find((x) => x.id === result);
         if (searchedOrganization) {
           this.currentOrganization = searchedOrganization;
         }
         else {
-          // eslint-disable-next-line no-console
-          console.log('error');
+          this.setOrganization();
         }
       });
+
+    if (this.storageService.currentOrganizationId === -1) {
+      return;
+    }
+
+    const searchedOrganization = this.availableOrganizations
+      .find((x) => x.id === this.storageService.currentOrganizationId);
+
+    if (searchedOrganization) {
+      this.currentOrganization = searchedOrganization;
+    }
+    else {
+      this.setOrganization();
+    }
+  }
+
+  private setOrganization(): void {
+    this.organizationService.getOrganization(this.storageService.currentOrganizationId)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(
+        (result) => {
+          if (result.body) {
+            this.currentOrganization = result.body;
+            this.availableOrganizations.push(this.currentOrganization);
+          }
+        }
+      );
   }
 
   @Input() public availableOrganizations: OrganizationModel[] = [];
