1857035c915c64543e1dda5a9b0e2e1bb0529afa
Daniil <daniilpoyarkov@gmail.com>
2022-08-11 09:54:16 +0300

    try to get value from custom input/not fully implemented

--- frontend/src/shared/components/register-page/register-page.component.ts
+++ frontend/src/shared/components/register-page/register-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register-page',
@@ -32,6 +32,18 @@ export class RegisterPageComponent implements OnInit {
 
   sendForm(){
     console.log(`data: ${this.userName} ${this.userEmail} ${this.userPassword}`);
+  }
+
+  recieveName(name: any) {
+    this.userName = name;
+  }
+
+  recieveEmail(email: any) {
+    this.userEmail = email;
+  }
+
+  recievePassword(password: any) {
+    this.userPassword = password;
   }
 
 }
