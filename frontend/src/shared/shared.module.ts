803d09158f3ce0a9529fd401d2b6c56ab51fa191
andpew <edarkmail@gmail.com>
2022-08-11 14:36:28 +0300

    Fixed extra tabs

--- frontend/src/shared/shared.module.ts
+++ frontend/src/shared/shared.module.ts
@@ -12,9 +12,7 @@ import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.com
 import { CreateOrganizationDialogComponent } from './components/create-organization/create-organization-dialog/create-organization-dialog.component';
 import { CreateOrganizationComponent } from './components/create-organization/create-organization.component';
 import { CreateProjectComponent } from './components/create-project/create-project.component';
-import {
-        CreateProjectDialogComponent
-} from './components/create-project/create-project-dialog/create-project-dialog.component';
+import { CreateProjectDialogComponent } from './components/create-project/create-project-dialog/create-project-dialog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ConfirmationModalComponent } from './components/tasque-confirmation-modal/confirmation-modal.component';
 import { OrganizationsDropdownComponent } from './components/organizations-dropdown/organizations-dropdown.component';
@@ -22,54 +20,54 @@ import { MenuDropdownComponent } from './components/tasque-menu-dropdown/menu-dr
 import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
-        imports: [
-                CommonModule,
-                RouterModule,
-                FormsModule,
-                ReactiveFormsModule,
-                RouterModule,
-                MaterialModule,
-                BrowserAnimationsModule,
-        ],
-        declarations: [
-                LoadingComponent,
-                ButtonComponent,
-                InputComponent,
-                CheckboxComponent,
-                CreateOrganizationComponent,
-                CreateOrganizationDialogComponent,
-                LeftSidebarComponent,
-                ConfirmationModalComponent,
-                DropdownComponent,
-                CreateProjectComponent,
-                CreateProjectDialogComponent,
-                LoginPageComponent,
-                OrganizationsDropdownComponent,
-                MenuDropdownComponent
-        ],
-        exports: [
-                CommonModule,
-                RouterModule,
-                FormsModule,
-                ReactiveFormsModule,
-                RouterModule,
-                MaterialModule,
-                BrowserAnimationsModule,
-                LoadingComponent,
-                ButtonComponent,
-                InputComponent,
-                CheckboxComponent,
-                ConfirmationModalComponent,
-                LeftSidebarComponent,
-                DropdownComponent,
-                CreateProjectComponent,
-                CreateProjectDialogComponent,
-                DropdownComponent,
-                CreateOrganizationComponent,
-                CreateOrganizationDialogComponent,
-                LoginPageComponent,
-                MenuDropdownComponent,
-                OrganizationsDropdownComponent
-        ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+    ],
+    declarations: [
+        LoadingComponent,
+        ButtonComponent,
+        InputComponent,
+        CheckboxComponent,
+        CreateOrganizationComponent,
+        CreateOrganizationDialogComponent,
+        LeftSidebarComponent,
+        ConfirmationModalComponent,
+        DropdownComponent,
+        CreateProjectComponent,
+        CreateProjectDialogComponent,
+        LoginPageComponent,
+        OrganizationsDropdownComponent,
+        MenuDropdownComponent
+    ],
+    exports: [
+        CommonModule,
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        LoadingComponent,
+        ButtonComponent,
+        InputComponent,
+        CheckboxComponent,
+        ConfirmationModalComponent,
+        LeftSidebarComponent,
+        DropdownComponent,
+        CreateProjectComponent,
+        CreateProjectDialogComponent,
+        DropdownComponent,
+        CreateOrganizationComponent,
+        CreateOrganizationDialogComponent,
+        LoginPageComponent,
+        MenuDropdownComponent,
+        OrganizationsDropdownComponent
+    ],
 })
 export class SharedModule { }
