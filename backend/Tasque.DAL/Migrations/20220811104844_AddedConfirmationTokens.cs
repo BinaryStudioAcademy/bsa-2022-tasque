8b70cb6870205fac561cc0e4064c62116e73498f
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-11 14:15:49 +0300

    #27 email confirmation (#32)
    * added base email structure
    
    * added mailJet options
    
    * added mailjet email service implementation
    
    * fixed error handling
    
    * added email registration confirmation
    
    * fixed nullref
    
    * added email confirmation model and respective migration
    
    * moved options classes to separate folder
    
    * added email confirmation options
    
    * added email confirmation endpoint
    
    * reimplemented exception handling mechanism
    
    * added custom EmailNotConfirmed exception and handler for it
    
    * fixed merge
    
    * removed unnessecary code from controller
    
    * fixed typo
    
    * Added confirmation email
    
    * updated readme database schema
    
    * Update README.md
    
    * renamed token and added Kind property
    
    * updated migrations

---
+++ backend/Tasque.DAL/Migrations/20220811104844_AddedConfirmationTokens.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tasque.Core.DAL.Migrations
+{
+    public partial class AddedConfirmationTokens : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailConfirmed",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "ConfirmationTokens",
+                columns: table => new
+                {
+                    Token = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    ExpiringAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Kind = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConfirmationTokens", x => x.Token);
+                    table.ForeignKey(
+                        name: "FK_ConfirmationTokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConfirmationTokens_UserId",
+                table: "ConfirmationTokens",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConfirmationTokens");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailConfirmed",
+                table: "Users");
+        }
+    }
+}
