ed18db67bc3bed69ab5fece534d0a35026c6f61a
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-11 21:20:21 +0300

    set up routing

--- frontend/src/app/components/auth-page/auth-page.component.ts
+++ frontend/src/app/components/auth-page/auth-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-page',
   templateUrl: './auth-page.component.html',
-  styleUrls: ['./auth-page.component.sass']
+  styleUrls: ['./auth-page.component.sass'],
 })
 export class AuthPageComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    if (!this.route.snapshot.firstChild) {
+      this.router.navigateByUrl('/auth/login');
+    }
   }
-
 }
