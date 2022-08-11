0a42d6529b4834de1574e7e8aac237573c501f3e
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-08-11 21:00:28 +0300

    button and input were added to select-user

---
+++ frontend/src/app/select-users/select-users.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-users',
+  templateUrl: './select-users.component.html',
+  styleUrls: ['./select-users.component.sass']
+})
+export class SelectUsersComponent implements OnInit {
+
+  public inputWidth = 683;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
