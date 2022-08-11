ebaf6fc9195a82804872423d225c54cde5e6c0b8
Daniil <daniilpoyarkov@gmail.com>
2022-08-11 17:09:37 +0300

    removed useless import

--- frontend/src/shared/components/register-page/register-page.component.ts
+++ frontend/src/shared/components/register-page/register-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
 import { faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
