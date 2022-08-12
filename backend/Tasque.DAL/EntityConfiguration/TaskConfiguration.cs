176abf4dfc6237a710681ba16bb2dd3cfc7a8e89
Daniil <daniilpoyarkov@gmail.com>
2022-08-12 16:57:57 +0300

    Created index entity User property email and placed to a separate folder all entities configuration

---
+++ backend/Tasque.DAL/EntityConfiguration/TaskConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tasque.Core.Common.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Task = Tasque.Core.Common.Entities.Task;
+
+namespace Tasque.Core.DAL.EntityConfiguration
+{
+    public class TaskConfiguration : IEntityTypeConfiguration<Task>
+    {
+        public void Configure(EntityTypeBuilder<Task> builder)
+        {
+            builder
+                .HasOne(task => task.Author)
+                .WithMany(user => user.OwnedTasks)
+                .HasForeignKey(task => task.AuthorId);
+
+            builder
+                .HasOne(task => task.LastUpdatedBy);
+        }
+    }
+}
