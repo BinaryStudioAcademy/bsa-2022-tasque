5767f9022b95b8a55ebb3328a2cf66e7d67bbecd
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-12 19:22:43 +0300

    added reset password logics

--- frontend/src/app/auth/components/reset-page/reset-page.component.ts
+++ frontend/src/app/auth/components/reset-page/reset-page.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { AuthService } from 'src/core/services/auth.service';
+import { ValidationConstants } from 'src/entity-models/const-resources/validation-constraints';
+import { LocalStorageKeys } from 'src/entity-models/local-storage-keys';
+import { UserResetPasswordModel } from 'src/entity-models/user-reset-password-model';
 
 @Component({
   selector: 'app-reset-page',
   templateUrl: './reset-page.component.html',
-  styleUrls: ['./reset-page.component.sass']
+  styleUrls: ['./reset-page.component.sass'],
 })
 export class ResetPageComponent implements OnInit, OnDestroy {
   public isValidKey = false;
@@ -16,15 +19,28 @@ export class ResetPageComponent implements OnInit, OnDestroy {
   public passwordRepeat = '';
   public hidePass = true;
   public hidePassRepeat = true;
-  public loginForm: FormGroup = new FormGroup({});
-  public emailControl: FormControl;
+  public resetPasswordForm: FormGroup = new FormGroup({});
+  public passwordControl: FormControl;
+  public passwordRepeatControl: FormControl;
+  public localStorage = window.localStorage;
+  public localStorageKeys = LocalStorageKeys;
 
   private unsubscribe$ = new Subject<void>();
+  private token: string;
 
-  constructor(
-    private route: ActivatedRoute,
-    private authService: AuthService,
-  ) {}
+  constructor(private route: ActivatedRoute, private authService: AuthService) {
+    this.passwordControl = new FormControl(this.password, [
+      Validators.required,
+      Validators.minLength(ValidationConstants.minLengthPassword),
+    ]);
+    this.passwordRepeatControl = new FormControl(this.passwordRepeat, [
+      Validators.required,
+    ]);
+    this.resetPasswordForm = new FormGroup({
+      passwordControl: this.passwordControl,
+      passwordRepeatControl: this.passwordRepeatControl,
+    });
+  }
 
   ngOnInit(): void {
     this.route.params.pipe(takeUntil(this.unsubscribe$)).subscribe((value) => {
@@ -32,6 +48,7 @@ export class ResetPageComponent implements OnInit, OnDestroy {
       if (!key) return;
       this.authService.confirmResetKey(key).subscribe((res) => {
         this.isValidKey = res.ok;
+        this.token = key;
       });
     });
   }
@@ -39,5 +56,37 @@ export class ResetPageComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
+  }
+
+  resetPasswordControl(): void {
+    this.passwordRepeatControl = new FormControl(this.passwordRepeat, [
+      Validators.required,
+      Validators.pattern(this.password as string),
+    ]);
+    this.resetPasswordForm = new FormGroup({
+      passwordControl: this.passwordControl,
+      passwordRepeatControl: this.passwordRepeatControl,
+    });
+  }
+
+  submit(): void {
+    const credentials = {
+      password: this.password,
+      token: this.token,
+    } as UserResetPasswordModel;
+
+    this.authService
+      .resetPassword(credentials)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((resp) => {
+        if (resp.ok) {
+          const token = resp.body;
+          this.localStorage.setItem(
+            this.localStorageKeys.token,
+            token?.accessToken as string,
+          );
+          // redirect into app here
+        }
+      });
   }
 }
