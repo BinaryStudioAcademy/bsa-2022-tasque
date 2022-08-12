1bb4c3829cb33df5e811e77a592f0210f02d66e6
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-12 20:03:35 +0300

    fixed module declarations

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -4,9 +4,17 @@ import { SharedModule } from 'src/shared/shared.module';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 import { AuthPageComponent } from './components/auth-page/auth-page.component';
+import { RestorePageComponent } from './components/restore-page/restore-page.component';
+import { ResetPageComponent } from './components/reset-page/reset-page.component';
 
 @NgModule({
-  declarations: [LoginPageComponent, RegisterPageComponent, AuthPageComponent],
+  declarations: [
+    LoginPageComponent,
+    RegisterPageComponent,
+    AuthPageComponent,
+    RestorePageComponent,
+    ResetPageComponent,
+  ],
   imports: [CommonModule, SharedModule],
 })
 export class AuthModule {}
