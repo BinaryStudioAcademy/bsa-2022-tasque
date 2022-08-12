b281b42d1048fc6c8d1e23eaa2c9c0e7eb72cf26
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-12 16:16:49 +0300

    created password restore request page

--- frontend/src/app/auth/components/restore-page/restore-page.component.ts
+++ frontend/src/app/auth/components/restore-page/restore-page.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { ValidationConstants } from 'src/entity-models/const-resources/validation-constraints';
 
 @Component({
   selector: 'app-restore-page',
   templateUrl: './restore-page.component.html',
-  styleUrls: ['./restore-page.component.sass']
+  styleUrls: ['./restore-page.component.sass'],
 })
 export class RestorePageComponent implements OnInit {
+  faArrow = faArrowLeft;
+  public email = '';
+  public loginForm: FormGroup = new FormGroup({});
+  public emailControl: FormControl;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.emailControl = new FormControl(this.email, [
+      Validators.required,
+      Validators.pattern(ValidationConstants.emailRegex),
+    ]);
   }
 
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      emailControl: this.emailControl,
+    });
+  }
 }
