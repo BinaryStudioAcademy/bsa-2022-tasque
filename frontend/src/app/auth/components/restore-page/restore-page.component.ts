6609dc7f34693e91a0d9e16ff7342473c859a354
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-12 18:47:45 +0300

    added submit to restore page

--- frontend/src/app/auth/components/restore-page/restore-page.component.ts
+++ frontend/src/app/auth/components/restore-page/restore-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { ToastrService } from 'ngx-toastr';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { AuthService } from 'src/core/services/auth.service';
 import { ValidationConstants } from 'src/entity-models/const-resources/validation-constraints';
 
 @Component({
@@ -13,8 +17,12 @@ export class RestorePageComponent implements OnInit {
   public email = '';
   public loginForm: FormGroup = new FormGroup({});
   public emailControl: FormControl;
+  public unsubscribe$ = new Subject<void>();
 
-  constructor() {
+  constructor(
+    private authService: AuthService,
+    private toastrService: ToastrService,
+  ) {
     this.emailControl = new FormControl(this.email, [
       Validators.required,
       Validators.pattern(ValidationConstants.emailRegex),
@@ -25,5 +33,16 @@ export class RestorePageComponent implements OnInit {
     this.loginForm = new FormGroup({
       emailControl: this.emailControl,
     });
+  }
+
+  public submit(): void {
+    if (!this.loginForm.valid) return;
+    const email = this.emailControl.value;
+    this.authService
+      .requestPasswordReset(email)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((resp) => {
+        this.toastrService.success(resp.body as string);
+      });
   }
 }
