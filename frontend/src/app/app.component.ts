dc578fc2938176a210581b3d483a9dd36423d955
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-12 09:39:14 +0300

    Fixed conflicts, removed unnecessary empty lines

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SideBarService } from 'src/core/services/sidebar.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tasque';
+  constructor(private sideBarService: SideBarService) {}
+
+  open() {
+    this.sideBarService.toggle();
+  }
 }
