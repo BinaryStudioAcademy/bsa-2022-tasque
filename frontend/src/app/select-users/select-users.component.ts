48fa452172a4dbf58e1a16b41d3d0c1a29aeea25
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-08-14 14:45:51 +0300

    User list was finished

--- frontend/src/app/select-users/select-users.component.ts
+++ frontend/src/app/select-users/select-users.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SelectUsersComponent implements OnInit {
 
   public inputWidth = 683;
+  public roles: string[] = ["Participant", "Admin", "Organizer"]
 
   constructor() { }
 
