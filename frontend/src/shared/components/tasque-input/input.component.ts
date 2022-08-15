3f4097e253ef75f5dfdf4d1f8070320f8cb29598
Setup1337 <thevadimchanel@gmail.com>
2022-08-15 09:06:58 +0200

    Added changes from previous PR, added types and width

--- frontend/src/shared/components/tasque-input/input.component.ts
+++ frontend/src/shared/components/tasque-input/input.component.ts
@@ -1,20 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'tasque-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.sass']
+  styleUrls: ['./input.component.sass'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => InputComponent)
+    }
+  ]
 })
-export class InputComponent implements OnInit {
-  public inputWidth = 100;
-  public inputBorderRadius = 5;
-  public inputClass = 'input';
+export class InputComponent implements ControlValueAccessor  {
+  public inputBorderRadius = 10;
+  public inputClass = '';
   public inputType = 'text';
-  public inputText = ''; 
+  public inputValue = '';
   public inputLabel = '';
   public inputPlaceholder = '';
   public inputErrorMessage = 'error';
-  public inputValueisError = false;
+  public inputValueIsError = false;
+
+  @Input() value: string;
 
   @Input()
   set type(typeName: string) {
@@ -25,24 +34,8 @@ export class InputComponent implements OnInit {
   }
 
   @Input()
-  set value(text: string) {
-    this.inputText = text;
-  }
-  get value(): string {
-    return this.inputText;
-  }
-
-  @Input()
-  set width(width: number){
-    this.inputWidth = width;
-  }
-  get width(): number{
-    return this.inputWidth;
-  }
-
-  @Input()
-  set borderRadius(raduis: number){
-    this.inputBorderRadius = raduis;
+  set borderRadius(radius: number){
+    this.inputBorderRadius = radius;
   }
   get borderRadius(): number{
     return this.inputBorderRadius;
@@ -73,24 +66,37 @@ export class InputComponent implements OnInit {
   }
 
   @Input()
-  set isValid(hasError: boolean) {
-    this.inputValueisError = !hasError;
+  set invalid(hasError: boolean) {
+    this.inputValueIsError = !hasError;
   }
-  get isValid(): boolean {
-    return this.inputValueisError;
+  get invalid(): boolean {
+    return this.inputValueIsError;
   }
 
   @Input()
   set errorMessage(message: string) {
     this.inputErrorMessage = message;
   }
-  get(): string {
+  get errorMessage(): string {
     return this.inputErrorMessage;
   }
 
+  onChange: (value: Event) => void = () => {};
+
+  onTouched: (value: Event) => void = () => {};
+
   constructor() { }
 
-  ngOnInit(): void {
+  writeValue(value: string): void {
+    this.inputValue = value;
   }
-  
+
+  registerOnChange(fn: (value: Event) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: (value: Event) => void): void {
+    this.onTouched = fn;
+  }
+
 }
