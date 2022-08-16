47dbd6990604772a005f4f0a84d6440efe029602
andpew <edarkmail@gmail.com>
2022-08-16 11:26:21 +0300

    Added task model

---
+++ frontend/src/core/models/task/task-model.ts
@@ -0,0 +1,22 @@
+import { TaskPriority } from '../enums/task-priority';
+import { TaskState } from '../enums/task-state';
+import { TaskType } from '../enums/task-type';
+
+export interface TaskModel {
+    id: number,
+    name: string,
+    description?: string,
+    summary: string,
+    taskState: TaskState,
+    taskType: TaskType,
+    taskPriority: TaskPriority,
+    authorId: number,
+    projectId: number,
+    sprintId: number,
+    lastUpdatedById: number,
+    parentTaskId: number,
+    createdAt: Date,
+    updatedAt: Date,
+    deadLine: Date,
+    finishedAt?: Date,
+}
