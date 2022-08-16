a59ac07ea6948657f5d40c2c8d0eaa0834825353
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-16 16:10:37 +0300

    added  rich-text-editor .ts

--- frontend/src/shared/components/tasque-rich-text-editor/rich-text-editor.component.ts
+++ frontend/src/shared/components/tasque-rich-text-editor/rich-text-editor.component.ts
@@ -9,4 +9,20 @@ export class RichTextEditorComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  public undo(): void {
+    document.execCommand('undo');
+  }
+
+  public redo(): void {
+    document.execCommand('redo');
+  }
+
+  public setList(style: string): void {
+    document.execCommand(style, false, 'NewUL');
+  }
+
+  public setfontSize(value: string): void {
+    document.execCommand('fontSize', true, value);
+  }
 }
