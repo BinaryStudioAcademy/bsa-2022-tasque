16207442f6e4bcce6e371264fdd32a6df8d8b94f
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-16 12:13:01 +0300

    added logics to login form

--- frontend/src/app/auth/components/login-page/login-page.component.ts
+++ frontend/src/app/auth/components/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons';
@@ -8,6 +8,8 @@ import { AuthService } from 'src/core/services/auth.service';
 import { LocalStorageKeys } from 'src/entity-models/local-storage-keys';
 import { UserLoginModel } from 'src/entity-models/user-login-model';
 import { ValidationConstants } from 'src/entity-models/const-resources/validation-constraints';
+import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
+import { InputComponent } from 'src/shared/components/tasque-input/input.component';
 
 @Component({
   selector: 'app-login-page',
@@ -17,6 +19,8 @@ import { ValidationConstants } from 'src/entity-models/const-resources/validatio
 export class LoginPageComponent implements OnInit, OnDestroy {
   faGithub = faGithub;
   faGoogle = faGoogle;
+  faHide = faEye;
+  faShow = faEyeSlash;
 
   public userLogin: UserLoginModel = {};
   public hidePass = true;
@@ -29,15 +33,42 @@ export class LoginPageComponent implements OnInit, OnDestroy {
   public localStorageKeys = LocalStorageKeys;
   private validationConstants = ValidationConstants;
 
+  @ViewChild('passwordInput') passwordInput: InputComponent;
+
+  get emailErrorMessage(): string {
+    const ctrl = this.emailControl;
+    if (ctrl.errors?.['required'] && (ctrl.dirty || ctrl.touched)) {
+      return 'Email is required';
+    }
+    if (ctrl.errors?.['pattern']) {
+      return 'Incorrect email format';
+    }
+
+    return '';
+  }
+
+  get passwordErrorMessage(): string {
+    const ctrl = this.passwordControl;
+    if (ctrl.errors?.['required'] && (ctrl.dirty || ctrl.touched)) {
+      return 'Email is required';
+    }
+    if (ctrl.errors?.['minlength']) {
+      return 'Password must be at least 8 characters';
+    }
+
+    return '';
+  }
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
 
     private authService: AuthService,
-  ) {this.emailControl = new FormControl( this.userLogin.email, [
+  ) {
+    this.emailControl = new FormControl(this.userLogin.email, [
       Validators.email,
       Validators.required,
-      Validators.pattern(this.validationConstants.emailRegex)
+      Validators.pattern(this.validationConstants.emailRegex),
     ]);
     this.passwordControl = new FormControl(this.userLogin.password, [
       Validators.required,
@@ -71,13 +102,28 @@ export class LoginPageComponent implements OnInit, OnDestroy {
   }
 
   public submitForm(): void {
-    this.authService.loginUser(this.userLogin)
-    .pipe(takeUntil(this.unsubscribe$))
-    .subscribe((resp) => {
-      if(resp.ok){
-        const token = resp.body;
-        this.localStorage.setItem(this.localStorageKeys.token, token?.accessToken as string);
-      }
-    });
+    this.userLogin = {
+      email: this.loginForm.get('emailControl')?.value,
+      password: this.loginForm.get('passwordControl')?.value,
+    };
+
+    this.authService
+      .loginUser(this.userLogin)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((resp) => {
+        if (resp.ok) {
+          const token = resp.body;
+          this.localStorage.setItem(
+            this.localStorageKeys.token,
+            token?.accessToken as string,
+          );
+        }
+      });
+  }
+
+  flipPasswordVisible(): void {
+    this.hidePass = !this.hidePass;
+    this.passwordInput.type = this.hidePass ? 'password' : 'text';
+    this.passwordInput.icon = this.hidePass ? this.faHide : this.faShow;
   }
 }
