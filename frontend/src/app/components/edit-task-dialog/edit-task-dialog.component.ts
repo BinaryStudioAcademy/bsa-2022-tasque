8d416818ed528793d6b6d48e980c967beea891c3
andpew <edarkmail@gmail.com>
2022-08-16 15:43:48 +0300

    Updated dialog to be the right side of the screen

--- frontend/src/app/components/edit-task-dialog/edit-task-dialog.component.ts
+++ frontend/src/app/components/edit-task-dialog/edit-task-dialog.component.ts
@@ -1,10 +1,13 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { TaskModel } from 'src/core/models/task/task-model';
 import { UserModel } from 'src/core/models/user/user-model';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { BaseComponent } from 'src/core/base/base.component';
 
 export interface EditTaskDialogData {
   currentUser: UserModel,
-  task: Task
+  task: TaskModel
 }
 
 @Component({
@@ -12,9 +15,17 @@ export interface EditTaskDialogData {
   templateUrl: './edit-task-dialog.component.html',
   styleUrls: ['./edit-task-dialog.component.sass']
 })
-export class EditTaskDialogComponent implements OnInit {
+export class EditTaskDialogComponent extends BaseComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public currentUser: UserModel) { }
+  public task: TaskModel;
+  public currentUser: UserModel;
+  public buttonIcon = faXmark;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: EditTaskDialogData) {
+    super();
+    this.task = data.task;
+    this.currentUser = data.currentUser;
+  }
 
   ngOnInit(): void { }
 }
