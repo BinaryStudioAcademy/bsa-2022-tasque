413bf39e3b57ef381d8ce3fb039fadeeb6dd395e
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-16 16:03:33 +0300

    added tasque-rich-text-editor component

---
+++ frontend/src/shared/components/tasque-rich-text-editor/rich-text-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tasque-rich-text-editor',
+  templateUrl: './rich-text-editor.component.html',
+  styleUrls: ['./rich-text-editor.component.sass'],
+})
+export class RichTextEditorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
