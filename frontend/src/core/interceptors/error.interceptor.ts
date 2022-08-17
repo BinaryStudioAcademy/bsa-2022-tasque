252e31f620df3e4c34459f693fca21596ec9595a
Setup1337 <thevadimchanel@gmail.com>
2022-08-17 15:07:00 +0200

    Add toastr notifications for interceptor

--- frontend/src/core/interceptors/error.interceptor.ts
+++ frontend/src/core/interceptors/error.interceptor.ts
@@ -1,17 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpHandler, HttpInterceptor, HttpErrorResponse, HttpRequest, HttpEvent } from '@angular/common/http';
+import {
+  HttpHandler,
+  HttpInterceptor,
+  HttpErrorResponse,
+  HttpRequest,
+  HttpEvent,
+} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { NotificationService } from '../services/notification.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor() { }
+    constructor(private notificationService: NotificationService) {
+      this.notificationService.position = 'toast-top-right';
+    }
     handleError(error: HttpErrorResponse): Observable<never> {
         return throwError(new Error(error.message ?? 'Something went wrong'));
     }
     intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        return next.handle(req).pipe(catchError(this.handleError));
+        return new Observable((observer) => {
+          next.handle(req).subscribe(
+            (res: HttpEvent<unknown>) => {
+              observer.next(res);
+            },
+            (err: HttpErrorResponse) => {
+              this.notificationService.error(err.message);
+            },
+          );
+        });
     }
-} 
+}
