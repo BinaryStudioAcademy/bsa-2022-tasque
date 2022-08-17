ea8bb5a23b6b69975498b7bc93ee11a0deba7b80
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-17 12:08:18 +0300

    updated error interceptor

--- frontend/src/core/interceptors/error.interceptor.ts
+++ frontend/src/core/interceptors/error.interceptor.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpHandler, HttpInterceptor, HttpErrorResponse, HttpRequest, HttpEvent } from '@angular/common/http';
+import {
+  HttpHandler,
+  HttpInterceptor,
+  HttpErrorResponse,
+  HttpRequest,
+  HttpEvent,
+} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor() { }
-    handleError(error: HttpErrorResponse): Observable<never> {
-        return throwError(new Error(error.message ?? 'Something went wrong'));
+  constructor() {}
+  handleError(error: HttpErrorResponse): Observable<never> {
+    if (error.status >= 400 && error.status < 500) {
+      return throwError(error);
     }
-    intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        return next.handle(req).pipe(catchError(this.handleError));
-    }
-} 
+    return throwError(new Error(error.message ?? 'Something went wrong'));
+  }
+
+  intercept(
+    req: HttpRequest<unknown>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(req).pipe(catchError(this.handleError));
+  }
+}
