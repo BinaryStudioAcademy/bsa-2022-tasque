94f501126a4c5f970085353b1bb8ae303fdb4e59
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-18 11:10:17 +0300

    added sendgrid service template and moved mailhet service into one folder

---
+++ backend/Tasque.BLL/Services/Email/SendGridService.cs
@@ -0,0 +1,22 @@
+﻿using Tasque.Core.Common.Models.Email;
+
+namespace Tasque.Core.BLL.Services.Email
+{
+    public class SendGridService : IEmailService
+    {
+        public Task<bool> SendEmailAsync(EmailMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SendEmailsAsync(IEnumerable<EmailMessage> messages)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SendEmailsAsync(EmailContact sender, IEnumerable<EmailContact> recievers, string subject, string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
