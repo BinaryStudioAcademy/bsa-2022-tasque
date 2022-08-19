f2ace71caa0380fcbf4bde312a6ebc2e4d0b9f7b
Daniil <101814817+DaniilPoiarkov@users.noreply.github.com>
2022-08-19 17:22:59 +0300

    Delete AWSKeys.cs

--- backend/Tasque.WebAPI/AppConfigurationExtension/ConstResources/AWSKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tasque.Core.WebAPI.AppConfigurationExtension.ConstResources
-{
-    public static class AWSKeys
-    {
-        public const string AWSProfile = "dynamo_user";
-        public const string AccessKey = "AKIAVB25LANXBI6DLR72";
-        public const string SecretKey = "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";
-    }
-}
