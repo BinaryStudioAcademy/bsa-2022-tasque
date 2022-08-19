08e12ce16583d97971e983119639ab51defb48aa
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-19 15:57:51 +0300

    added

--- frontend/src/app/components/edit-organization/edit-organization-dialog/edit-organization-dialog.component.ts
+++ frontend/src/app/components/edit-organization/edit-organization-dialog/edit-organization-dialog.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-edit-organization-dialog',
   templateUrl: './edit-organization-dialog.component.html',
-  styleUrls: ['./edit-organization-dialog.component.sass']
+  styleUrls: ['./edit-organization-dialog.component.sass'],
 })
 export class EditOrganizationDialogComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  public createBtnName = 'Create';
+  public btnClass = 'mini';
+  public cancelBtnName = 'Cancel';
+  public inputClass = 'input';
+  public placeholderText = 'Write organization name';
+  public inputType = 'text';
+  public inputLabel = 'Organization name';
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
