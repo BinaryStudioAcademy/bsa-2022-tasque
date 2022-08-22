e2cb1e5f403ffa7ca825e76eb6ba3367e4647e60
andpew <edarkmail@gmail.com>
2022-08-22 13:17:05 +0300

    Added configuration for UserProjectRole

---
+++ backend/Tasque.DAL/EntityConfiguration/UserProjectRoleConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tasque.Core.Common.Entities;
+
+namespace Tasque.Core.DAL.EntityConfiguration
+{
+    public class UserProjectRoleConfiguration : IEntityTypeConfiguration<UserProjectRole>
+    {
+        public void Configure(EntityTypeBuilder<UserProjectRole> builder)
+        {
+            builder
+                .HasNoKey();
+        }
+    }
+}
