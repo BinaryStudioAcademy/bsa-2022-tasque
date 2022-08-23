e733eede14d303355ff7009d3e35be16d854a254
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-23 20:10:17 +0300

    implemented not found page

--- frontend/src/shared/components/not-found-page/not-found-page.component.ts
+++ frontend/src/shared/components/not-found-page/not-found-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fa0, fa4 } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-not-found-page',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found-page.component.sass']
 })
 export class NotFoundPageComponent implements OnInit {
+
+  faFour = fa4;
+  faZero = fa0;
 
   constructor() { }
 
