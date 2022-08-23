a5195aacc8f9caa183f265de96b9a32773fc10e9
Setup1337 <thevadimchanel@gmail.com>
2022-08-23 14:15:56 +0200

    Removed unused imports

--- frontend/src/app/auth/guards/restore.guard.ts
+++ frontend/src/app/auth/guards/restore.guard.ts
@@ -1,19 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanDeactivate, Router, UrlTree } from '@angular/router';
+import { CanDeactivate, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { RestorePageComponent } from '../components/restore-page/restore-page.component';
-import { AuthService } from '../../../core/services/auth.service';
-import { NotificationService } from '../../../core/services/notification.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestoreGuard implements CanDeactivate<RestorePageComponent> {
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-    private notificationService: NotificationService,
-  ) {}
   canDeactivate(
     component: RestorePageComponent,
   ):
