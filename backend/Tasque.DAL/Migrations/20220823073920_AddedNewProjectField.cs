de1d08b61cc227adaca73bb19ab147404b91edca
andpew <edarkmail@gmail.com>
2022-08-23 10:40:30 +0300

    Added new migration

---
+++ backend/Tasque.DAL/Migrations/20220823073920_AddedNewProjectField.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tasque.Core.DAL.Migrations
+{
+    public partial class AddedNewProjectField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Key",
+                table: "Projects",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Key",
+                table: "Projects");
+        }
+    }
+}
