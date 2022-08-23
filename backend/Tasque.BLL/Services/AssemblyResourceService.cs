dfd14785b08d70ecf3d0283dd994c6a58c72e431
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-08-23 15:42:19 +0300

    112 email design (#106)
    * added service to extract app recources
    
    * added confirmation email template
    
    * changed confirmation token service to use assembly resources
    
    * fixed naming
    
    * added confirm email message
    
    * fixed email confirmation email text and flow
    
    * added fixme

---
+++ backend/Tasque.BLL/Services/AssemblyResourceService.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+
+namespace Tasque.Core.BLL.Services
+{
+    public static class AssemblyResourceService
+    {
+        private static readonly Dictionary<string, string> _resources = new();
+        public static async Task<string> GetResource(AssemblyResource res)
+        {
+            var name = res.FileName;
+            if (_resources.TryGetValue(name, out string? val))
+                return val;
+            var str = await Read(name);
+            _resources.Add(name, str);
+            return str;
+        }
+
+        private static async Task<string> Read(string name)
+        {
+            // Determine path
+            var assembly = Assembly.GetExecutingAssembly();
+            try
+            {
+                string resourcePath = name;
+                resourcePath = assembly
+                    .GetManifestResourceNames()
+                    .Single(str => str.EndsWith(name));
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourcePath)!)
+                using (StreamReader reader = new(stream))
+                {
+                    return await reader.ReadToEndAsync();
+                }
+            }
+            catch (Exception)
+            { return string.Empty; }
+        }
+    }
+
+    public sealed class AssemblyResource
+    {
+        internal string FileName { get; }
+        private AssemblyResource(string file) => FileName = file;
+        public static AssemblyResource ResetPasswordMessage => new("ResetPasswordMessage.html");
+        public static AssemblyResource ConfirmEmailMessage => new("ConfirmEmailMessage.html");
+    }
+}
