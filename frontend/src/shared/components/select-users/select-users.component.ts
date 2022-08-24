e7d010451df8ec6746944ed4909222cbcaf050ef
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-08-24 15:23:59 +0300

    Removed code for testing

--- frontend/src/shared/components/select-users/select-users.component.ts
+++ frontend/src/shared/components/select-users/select-users.component.ts
@@ -40,43 +40,6 @@ export class SelectUsersComponent implements OnInit {
           name,
         };
       });
-
-    // board should be passed as a parameter
-    // ================================
-    const boardName = prompt('Please, enter a board name (supported values - "WithRoles", "WithoutRoles")') as string;
-    const getBoard = (): IBoard => {
-      switch (boardName) {
-        case 'WithRoles':
-          return {
-            id: 1,
-            type: BoardType.Organization,
-            hasRoles: true,
-            users: [],
-          };
-        case 'WithoutRoles':
-          return {
-            id: 1,
-            type: BoardType.Board,
-            hasRoles: false,
-            users: [],
-          };
-        default:
-          throw TypeError('Unknown name');
-      }
-    };
-    const board = getBoard();
-    const key = this.service.createKey(board);
-
-    if (localStorage.getItem(key)) {
-      this.board = JSON.parse(localStorage.getItem(key) as string);
-    } else {
-      this.board = board;
-    }
-    // ================================
-
-    if (!this.board) {
-      throw new TypeError('Board is required');
-    }
   }
 
   ngOnInit(): void {
@@ -120,7 +83,7 @@ export class SelectUsersComponent implements OnInit {
   delete(email: string): void {
     this.isLoading = true;
     this.service.deleteUser(this.board, email).subscribe(() => {
-      this.toastr.success(`${this.userEmail} was deleted successfully !`);
+      this.toastr.success(`${email} was deleted successfully !`);
       this.refreshList();
     });
   }
