48c80af89004e8b433f98e09378e11506950dc9f
andpew <edarkmail@gmail.com>
2022-08-25 17:16:16 +0300

    Redesigned list of projects

--- frontend/src/app/components/project-list/project-list-item/project-list-item.component.ts
+++ frontend/src/app/components/project-list/project-list-item/project-list-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ProjectModel } from 'src/core/models/project/project-model';
+import { UserModel } from 'src/core/models/user/user-model';
 
 @Component({
   selector: 'app-project-list-item',
@@ -7,12 +9,10 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ProjectListItemComponent implements OnInit {
 
-  @Input()
-  public projectName: string;
+  @Input() public currentUser: UserModel;
+  @Input() public project: ProjectModel;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
 }
