aadfaff5411896a20c0e16e75e830678291b7e61
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-25 11:40:49 +0300

    added form validators

--- frontend/src/app/components/create-project/create-project-dialog/create-project-dialog.component.ts
+++ frontend/src/app/components/create-project/create-project-dialog/create-project-dialog.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, Inject, OnInit } from '@angular/core';
+﻿import { Component, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Subject } from 'rxjs';
 import { ProjectService } from '../../../../core/services/project.service';
 import { NewProjectModel } from '../../../../core/models/project/new-project-model';
@@ -6,13 +6,39 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { takeUntil } from 'rxjs/operators';
 import { NewProjectCredentialsModel } from '../../../../core/models/project/new-project-credentials.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { InputComponent } from 'src/shared/components/tasque-input/input.component';
 
 @Component({
   selector: 'app-create-project-dialog',
   templateUrl: './create-project-dialog.component.html',
-  styleUrls: ['./create-project-dialog.component.sass']
+  styleUrls: ['./create-project-dialog.component.sass'],
 })
-export class CreateProjectDialogComponent implements OnInit {
+export class CreateProjectDialogComponent implements OnInit, OnDestroy {
+  get projectNameErrorMessage(): string {
+    const ctrl = this.projectNameControl;
+
+    if (ctrl.errors?.['required']) {
+      return 'Project name is required';
+    }
+    return '';
+  }
+
+  get projectKeyErrorMessage(): string {
+    const ctrl = this.projectKeyControl;
+
+    if (ctrl.errors?.['required']) {
+      return 'Project key is required';
+    }
+    return '';
+  }
+
+  public createProjectForm: FormGroup = new FormGroup({});
+  public projectNameControl: FormControl;
+  public projectKeyControl: FormControl;
+
+  @ViewChild('projectNameInput') projectNameInput: InputComponent;
+  @ViewChild('projectKeyInput') projectKeyInput: InputComponent;
 
   public createBtnName = 'Create project';
   public createBtnClass = 'fill';
@@ -31,28 +57,77 @@ export class CreateProjectDialogComponent implements OnInit {
   public inputKeyLabel = 'Key';
 
   public newProjectName = '';
+  public newProjectKey = '';
 
   public unsubscribe$ = new Subject<void>();
+
+  public newProject: NewProjectModel;
 
   constructor(
     public projectService: ProjectService,
     @Inject(MAT_DIALOG_DATA) public data: NewProjectCredentialsModel,
-  ) { }
-
-  ngOnInit(): void {
-
+  ) {
+    this.projectNameControl = new FormControl(this.newProjectName, [
+      Validators.required,
+    ]);
+    this.projectKeyControl = new FormControl(this.newProjectKey, [
+      Validators.required,
+    ]);
   }
 
-  onSubmit(): void{
+  public showError(input: InputComponent): void {
+    let isError = false;
+    let errMsg = '';
+    switch (input.inputLabel) {
+      case 'Key':
+        isError = this.projectKeyControl.invalid;
+        errMsg = this.projectKeyErrorMessage;
+        break;
+      case 'Name':
+        isError = this.projectNameControl.invalid;
+        errMsg = this.projectNameErrorMessage;
+        break;
+    }
 
+    input.invalid = isError;
+    input.errorMessage = errMsg;
+  }
+
+  public hideError(input: InputComponent): void {
+    input.invalid = false;
+  }
+
+  ngOnInit(): void {
+    this.createProjectForm = new FormGroup({
+      projectNameControl: this.projectNameControl,
+      projectKeyControl: this.projectKeyControl,
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  onSubmit(): void {
+    if (!this.createProjectForm.valid) {
+      return;
+    }
+
+    if (this.createProjectForm.invalid) {
+      this.showError(this.projectKeyInput);
+      this.showError(this.projectNameInput);
+
+      return;
+    }
     const newProject: NewProjectModel = {
       name: this.newProjectName,
       authorId: this.data.authorId,
       organizationId: this.data.organizationId,
     };
-    this.projectService.createProject(newProject)
+    this.projectService
+      .createProject(newProject)
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe();
   }
-
 }
