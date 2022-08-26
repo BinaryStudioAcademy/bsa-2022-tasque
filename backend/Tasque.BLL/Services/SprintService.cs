631c878b821d54821fab39febe9b6f5e3fb55b3c
Oleksandr <oleksand.vovk@gmail.com>
2022-08-26 12:27:57 +0300

    Added SprintController

--- backend/Tasque.BLL/Services/SprintService.cs
+++ backend/Tasque.BLL/Services/SprintService.cs
@@ -8,27 +8,12 @@ using Tasque.Core.DAL;
 
 namespace Tasque.Core.BLL.Services
 {
-    public class SprintService
+    public class SprintService : EntityCrudService<Sprint>
     {
-        private DataContext _context;
-        private IMapper _mapper;
-
-        public SprintService(
-            DataContext context,
-            IMapper mapper)
+        public SprintService(DataContext db) : base(db)
         {
-            _context = context;
-            _mapper = mapper;
+
         }
 
-        public async Task<SprintDto> GetSprintById(int id)
-        {
-            if (!await _context.Sprints.AnyAsync(u => u.Id == id))
-            {
-                throw new ValidationException("Sprint with given id does not exist");
-            }
-            var sprintEntity = await _context.Sprints.FirstAsync(u => u.Id == id);
-            return _mapper.Map<SprintDto>(sprintEntity);
-        }
     }
 }
---
+++ backend/Tasque.Core.WebAPI/Controllers/SprintController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tasque.Core.BLL.Services;
+using Tasque.Core.Common.DTO;
+using Tasque.Core.Common.Entities;
+
+namespace Tasque.Core.WebAPI.Controllers;
+
+[Route("api/sprint")]
+public class SprintController : EntityController<Sprint, SprintDto, SprintService>
+{
+    public SprintController(SprintService service) : base(service)
+    {
+        
+    }
+}
