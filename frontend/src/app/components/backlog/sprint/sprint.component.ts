ae7104875c213dddf822289da3b8bb5fc5e5da1e
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-26 18:08:49 +0300

    created sprint header

--- frontend/src/app/components/backlog/sprint/sprint.component.ts
+++ frontend/src/app/components/backlog/sprint/sprint.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sprint',
   templateUrl: './sprint.component.html',
-  styleUrls: ['./sprint.component.sass']
+  styleUrls: ['./sprint.component.sass'],
 })
 export class SprintComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  faEllipsisV = faEllipsisV;
+  ngOnInit(): void {}
 }
