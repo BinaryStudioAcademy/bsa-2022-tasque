a70be2a0560e38388cc7fae3fb4b32a3c9e5d8a6
andpew <edarkmail@gmail.com>
2022-08-28 21:26:14 +0300

    Added navigation to project page and org choosing On button open click

--- frontend/src/app/components/organization-list/organization-list-item/organization-list-item.component.ts
+++ frontend/src/app/components/organization-list/organization-list-item/organization-list-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UserModel } from 'src/core/models/user/user-model';
 import { OrganizationModel } from 'src/core/models/organization/organization-model';
+import { Router } from '@angular/router';
+import { StorageService } from 'src/core/services/storage.service';
 
 @Component({
   selector: 'app-organization-list-item',
@@ -11,7 +13,16 @@ export class OrganizationListItemComponent implements OnInit {
   @Input() public currentUser: UserModel;
   @Input() public organization: OrganizationModel;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private storageService: StorageService
+  ) { }
 
   ngOnInit(): void { }
+
+  public openOrganization(): void {
+    this.storageService.currentOrganizationId = this.organization.id;
+    this.router.navigate(['projects'], { replaceUrl: true });
+    window.scroll(0, 0);
+  }
 }
