43188a4561ca1dc44963ed94dd34e1635a250ab8
Maxim <62253266+FeloniukM@users.noreply.github.com>
2022-08-30 12:38:29 +0300

    Card component (#141)
    * add card component
    
    * fix build

---
+++ frontend/src/app/components/tasque-card/tasque-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TaskInfoModel } from 'src/core/models/board/task-Info-model';
+
+@Component({
+  selector: 'tasque-card',
+  templateUrl: './tasque-card.component.html',
+  styleUrls: ['./tasque-card.component.sass']
+})
+export class TasqueCardComponent implements OnInit {
+
+  //Gets information about the task
+  @Input() taskInfo: TaskInfoModel; 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
