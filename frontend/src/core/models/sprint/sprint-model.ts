aeeaf4170ac5d75ed0ef576f3e7e2fd3932eca94
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-08-31 09:41:17 +0300

    delete task[] property

--- frontend/src/core/models/sprint/sprint-model.ts
+++ frontend/src/core/models/sprint/sprint-model.ts
@@ -1,10 +1,7 @@
-import { TaskModel } from '../task/task-model';
-
 export interface SprintModel {
   id: number;
   name: string;
   description?: string;
-  tasks?: TaskModel[];
   createdAt: Date;
   updatedAt: Date;
 }
