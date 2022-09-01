1bf098d6e83e28154596563c28de0346acee0fc4
unknown <daniilpoyarkov@gmail.com>
2022-09-01 14:20:53 +0300

    Fixed namespaces

--- backend/Tasque.Core.Common/Entities/TaskAdditionalModels/TaskCustomField.cs
+++ backend/Tasque.Core.Common/Entities/TaskAdditionalModels/TaskCustomField.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tasque.Core.Common.Entities.Task
+namespace Tasque.Core.Common.Entities.TaskAdditionalModels
 {
     public class TaskCustomField
     {
