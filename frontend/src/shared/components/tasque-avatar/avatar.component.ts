2ec8868bbd5c392fcf6f57f34ce9afe89e24c259
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-09-05 13:43:42 +0300

    avatar fixes

--- frontend/src/shared/components/tasque-avatar/avatar.component.ts
+++ frontend/src/shared/components/tasque-avatar/avatar.component.ts
@@ -1,13 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { UserModel } from 'src/core/models/user/user-model';
 
 @Component({
   selector: 'tasque-avatar',
   templateUrl: './avatar.component.html',
-  styleUrls: ['./avatar.component.sass']
+  styleUrls: ['./avatar.component.sass'],
 })
 export class AvatarComponent implements OnInit {
-
   @Input()
   diameter_px!: number;
 
@@ -17,20 +16,23 @@ export class AvatarComponent implements OnInit {
   @Input()
   user: UserModel;
 
-  fontSize: number;
-
-  constructor() { }
-
-  ngOnInit(): void {
-    this.fontSize = this.diameter_px / 2;
+  get fontSize(): number {
+    return this.diameter_px
+      ? this.diameter_px / 2
+      : this.el.nativeElement.offsetWidth / 2.5;
   }
 
-  getInitials(user: UserModel): string {
-    const partsOfName = user.name.split(' ');
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+  }
+
+  get initials(): string {
+    const partsOfName = this.user.name.split(' ');
     if (partsOfName.length >= 2) {
       return partsOfName[0][0] + partsOfName[1][0];
     }
-    
+
     return partsOfName[0][0];
   }
 }
