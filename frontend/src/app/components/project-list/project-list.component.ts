886c60cbb4ffbc1e77cb26a1675abfac9d9a69a5
FeloniukM <felonukmakcim@gmail.com>
2022-09-06 20:35:45 +0300

    fix conflict resolve

--- frontend/src/app/components/project-list/project-list.component.ts
+++ frontend/src/app/components/project-list/project-list.component.ts
@@ -6,7 +6,6 @@ import { UserModel } from 'src/core/models/user/user-model';
 import { GetCurrentOrganizationService } from 'src/core/services/get-current-organization.service';
 import { ProjectService } from 'src/core/services/project.service';
 import { GetCurrentUserService } from 'src/core/services/get-current-user.service';
-import { ProjectModel } from '../../../core/models/project/project-model';
 
 @Component({
   selector: 'app-project-list',
@@ -20,18 +19,15 @@ export class ProjectListComponent implements OnInit {
   public inputSearch = '';
   public searchIcon = faMagnifyingGlass;
 
-  public projectsModel: ProjectInfoModel[] = [];
+  public projects: ProjectInfoModel[] = [];
   public items: ProjectModel[] = [];
 
 
-  public itemsShow = this.projectsModel;
+  public itemsShow = this.projects;
 
   constructor(public projectService: ProjectService, 
-    private currentOrganization: GetCurrentOrganizationService) { 
-
-  constructor(
-    private currentUserService: GetCurrentUserService
-  ) {
+    private currentOrganization: GetCurrentOrganizationService,
+    private currentUserService: GetCurrentUserService) {
     this.currentUserService.currentUser.subscribe((res) => {
       this.currentUser = res as UserModel;
     });
@@ -40,20 +36,20 @@ export class ProjectListComponent implements OnInit {
   ngOnInit(): void {
     this.projectService.getAllProjectsOfThisOrganization(this.currentOrganization.currentOrganizationId).subscribe((data) => {
       if(data.body) {
-        this.projectsModel = data.body;
-        this.itemsShow = this.projectsModel;
+        this.projects = data.body;
+        this.itemsShow = this.projects;
       }
     });   
   }
 
   filterItems(): void {
     if (this.inputSearch) {
-      this.itemsShow = this.projectsModel.filter((item) => {
+      this.itemsShow = this.projects.filter((item) => {
         return item.name.toLowerCase().includes(this.inputSearch.toLowerCase());
       });
     }
     else {
-      this.itemsShow = this.projectsModel;
+      this.itemsShow = this.projects;
     }
   }
 
