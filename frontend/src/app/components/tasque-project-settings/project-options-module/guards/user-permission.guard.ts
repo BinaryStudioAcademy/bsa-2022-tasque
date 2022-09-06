1b3e07cd71d2de2ff86a90b5cc779a235c3ab9d6
unknown <daniilpoyarkov@gmail.com>
2022-09-06 17:55:42 +0300

    Added guard that checks if user has access to settings. If user has not access he will be redirected to 404 page

---
+++ frontend/src/app/components/tasque-project-settings/project-options-module/guards/user-permission.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { OrganizationModel } from 'src/core/models/organization/organization-model';
+import { UserModel } from 'src/core/models/user/user-model';
+import { GetCurrentOrganizationService } from 'src/core/services/get-current-organization.service';
+import { GetCurrentUserService } from 'src/core/services/get-current-user.service';
+import { OrganizationService } from 'src/core/services/organization.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserPermissionGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this.role >= 3){
+        return true;
+      }
+      this.router.navigate(['./not-found'])
+      return false;
+  }
+  
+  private currentUser: UserModel;
+  private organizationId: number;
+  private role: number;
+
+  constructor(
+    private router: Router,
+    private currentUserService: GetCurrentUserService,
+    private currentOrganizationService: GetCurrentOrganizationService,
+  ) { 
+    this.currentUserService.currentUser.subscribe((user) => {
+      this.currentUser = user as UserModel;
+
+      this.organizationId = this.currentOrganizationService.currentOrganizationId;
+      this.role = this.currentUser.organizationRoles?.find(r => r.organizationId === this.organizationId)?.role as number;
+    });
+  }
+}
