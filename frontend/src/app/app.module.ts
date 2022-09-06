23d73020825aef28354214635d5564ed208d2f8d
andpew <edarkmail@gmail.com>
2022-09-06 19:30:29 +0300

    Deleted create organization/project components And moved create dialogs to the shared folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,10 +9,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CreateOrganizationComponent } from './components/create-organization/create-organization.component';
-import { CreateProjectComponent } from './components/create-project/create-project.component';
-import { CreateOrganizationDialogComponent } from './components/create-organization/create-organization-dialog/create-organization-dialog.component';
-import { CreateProjectDialogComponent } from './components/create-project/create-project-dialog/create-project-dialog.component';
 import { AuthModule } from './auth/auth.module';
 import { ProjectListComponent } from './components/project-list/project-list.component';
 import { ProjectListItemComponent } from './components/project-list/project-list-item/project-list-item.component';
@@ -41,10 +37,6 @@ import { BacklogComponent } from './components/backlog/backlog.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CreateOrganizationComponent,
-    CreateOrganizationDialogComponent,
-    CreateProjectComponent,
-    CreateProjectDialogComponent,
     ProjectListComponent,
     ProjectListItemComponent,
     OrganizationListComponent,
