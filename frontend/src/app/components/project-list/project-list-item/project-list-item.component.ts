50d02b8433eb4bf5699a42848142a5ac0ac2c744
unknown <daniilpoyarkov@gmail.com>
2022-09-07 11:09:21 +0300

    Enhanced user role set on project list component

--- frontend/src/app/components/project-list/project-list-item/project-list-item.component.ts
+++ frontend/src/app/components/project-list/project-list-item/project-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 import { ProjectModel } from 'src/core/models/project/project-model';
 import { UserModel } from 'src/core/models/user/user-model';
 import { UserRole } from 'src/core/models/user/user-roles';
@@ -8,7 +8,7 @@ import { UserRole } from 'src/core/models/user/user-roles';
   templateUrl: './project-list-item.component.html',
   styleUrls: ['./project-list-item.component.sass']
 })
-export class ProjectListItemComponent implements OnInit {
+export class ProjectListItemComponent implements OnInit, OnChanges {
 
   @Input() public currentUser: UserModel;
   @Input() public project: ProjectModel;
@@ -17,7 +17,20 @@ export class ProjectListItemComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const model = this.currentUser.organizationRoles.find((m) => m.organizationId === this.project.organizationId)?.role as UserRole;
-    this.role = model;
+    if(this.currentUser === undefined) {
+      this.role = 0;
+    } else {
+      this.role = this.currentUser.organizationRoles
+        .find((m) => m.organizationId === this.project.organizationId)?.role as UserRole;
+    }
+  }
+
+  ngOnChanges(): void {
+    if(this.currentUser === undefined) {
+      this.role = 0;
+    } else {
+      this.role = this.currentUser.organizationRoles
+        .find((m) => m.organizationId === this.project.organizationId)?.role as UserRole;
+    }
   }
 }
