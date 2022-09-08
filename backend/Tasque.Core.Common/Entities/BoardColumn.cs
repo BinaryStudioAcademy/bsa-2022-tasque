74f16fc61732a9f96896ab409f8d15322e99c277
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-09-08 16:52:29 +0300

    updated model and dto

--- backend/Tasque.Core.Common/Entities/BoardColumn.cs
+++ backend/Tasque.Core.Common/Entities/BoardColumn.cs
@@ -4,8 +4,12 @@ namespace Tasque.Core.Common.Entities;
 
 public class BoardColumn : BaseEntity
 {
-    public string Name { get; set; } = null!;
-
+    public BoardColumn()
+    {
+        Tasks = new List<Task>();
+    }
     public int ProjectId { get; set; }
     public Project Project { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public virtual ICollection<Task> Tasks { get; set; }
 }
