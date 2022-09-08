22613f000bc9523c9a86617f6a37c13d0cdbbe88
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-09-08 14:43:10 +0300

    fixed build

--- frontend/src/app/components/tasque-team/tasque-team.component.ts
+++ frontend/src/app/components/tasque-team/tasque-team.component.ts
@@ -16,84 +16,98 @@ export class TasqueTeamComponent implements OnInit {
       avatarURL: 'https://api.tinyfox.dev/img?animal=fox',
       name: 'Test User',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 2,
       avatarURL: 'https://api.tinyfox.dev/img?animal=yeen',
       name: 'Test User 2',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 3,
       avatarURL: 'https://api.tinyfox.dev/img?animal=snek',
       name: 'Test User 3',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 4,
       avatarURL: 'https://api.tinyfox.dev/img?animal=poss',
       name: 'Test User 4',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 4,
       avatarURL: 'https://api.tinyfox.dev/img?animal=leo',
       name: 'Test User 5',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 4,
       avatarURL: 'https://api.tinyfox.dev/img?animal=serval',
       name: 'Test User 6',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 4,
       avatarURL: 'https://api.tinyfox.dev/img?animal=racc',
       name: 'Test User 7',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 1,
       avatarURL: 'https://api.tinyfox.dev/img?animal=bear',
       name: 'Test User 8',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 2,
       avatarURL: 'https://api.tinyfox.dev/img?animal=caracal',
       name: 'Test User 9',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 3,
       avatarURL: 'https://api.tinyfox.dev/img?animal=tig',
       name: 'Test User 10',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 4,
       avatarURL: 'https://api.tinyfox.dev/img?animal=puma',
       name: 'Test User 11',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 4,
       avatarURL: 'https://api.tinyfox.dev/img?animal=yote',
       name: 'Test User 12',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 4,
       avatarURL: 'https://api.tinyfox.dev/img?animal=jaguar',
       name: 'Test User 13',
       email: '',
+      organizationRoles: [],
     },
     {
       id: 4,
       avatarURL: 'https://api.tinyfox.dev/img?animal=capy',
       name: 'Test User 14',
       email: '',
+      organizationRoles: [],
     },
   ];
   public visible: UserModel[];
@@ -107,7 +121,7 @@ export class TasqueTeamComponent implements OnInit {
 
   public filter(val: string): void {
     this.visible = this.loaded.filter((user) =>
-      user.name.toLowerCase().includes(val.toLowerCase()),
+      user.name?.toLowerCase().includes(val.toLowerCase()),
     );
   }
 }
