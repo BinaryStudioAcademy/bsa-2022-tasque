5a0cc2f5c488e600a5dd6f0aa69b9789d01192c0
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-09-08 22:18:25 +0300

    SubscriptionManagement was implemented

---
+++ backend/Tasque.Messaging/SubscriptionManagement/InMemorySubscriptionsManager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Tasque.Core.Common.Entities;
+using Tasque.Core.Common.Models;
+using Tasque.Messaging.Abstractions;
+
+namespace Tasque.Messaging.SubscriptionManagement
+{
+    public class InMemorySubscriptionsManager : ISubscriptionsManager
+    {
+        private readonly Dictionary<string, List<SubscriptionInfo>> _handlers;
+        private readonly List<Type> _eventTypes;
+
+        public event EventHandler<string> OnEventRemoved;
+
+        public InMemorySubscriptionsManager()
+        {
+            _handlers = new Dictionary<string, List<SubscriptionInfo>>();
+            _eventTypes = new List<Type>();
+        }
+
+        public bool IsEmpty => !_handlers.Keys.Any();
+        public void Clear() => _handlers.Clear();
+
+        public void AddSubscription<TEvent, THandler>() where TEvent : IntegrationEvent where THandler : IIntegrationEventHandler<TEvent>
+        {
+            var eventName = GetEventKey<TEvent>();
+            var handlerType = typeof(THandler);
+
+            if (!EventHasSubscribers(eventName))
+            {
+                _handlers.Add(eventName, new List<SubscriptionInfo>());
+            }
+
+            if (_handlers[eventName].Any(s => s.HandlerType == handlerType))
+            {
+                throw new ArgumentException($"Handler Type {handlerType.Name} already registered for '{eventName}'", nameof(handlerType));
+            }
+
+            _handlers[eventName].Add(new SubscriptionInfo(handlerType));
+
+            if (!_eventTypes.Contains(typeof(TEvent)))
+            {
+                _eventTypes.Add(typeof(TEvent));
+            }
+        }
+
+        public void RemoveSubscription<TEvent, THandler>() where THandler : IIntegrationEventHandler<TEvent> where TEvent : IntegrationEvent
+        {
+            var subscriptionToRemove = FindSubscriptionToRemove<TEvent, THandler>();
+            var eventName = GetEventKey<TEvent>();
+
+            if (subscriptionToRemove != null)
+            {
+                _handlers[eventName].Remove(subscriptionToRemove);
+                if (!_handlers[eventName].Any())
+                {
+                    _handlers.Remove(eventName);
+                    var eventType = _eventTypes.SingleOrDefault(e => e.Name == eventName);
+                    if (eventType != null)
+                    {
+                        _eventTypes.Remove(eventType);
+                    }
+                    RaiseOnEventRemoved(eventName);
+                }
+            }
+        }
+
+        public IEnumerable<SubscriptionInfo> GetHandlersForEvent<TEvent>() where TEvent : IntegrationEvent
+        {
+            var key = GetEventKey<TEvent>();
+            return GetHandlersForEvent(key);
+        }
+
+        public IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName) => _handlers[eventName];
+
+        public bool HasSubscriptionsForEvent<TEvent>() where TEvent : IntegrationEvent
+        {
+            var key = GetEventKey<TEvent>();
+            return EventHasSubscribers(key);
+        }
+
+        public bool EventHasSubscribers(string eventName) => _handlers.ContainsKey(eventName);
+
+        public Type GetEventTypeByName(string eventName) => _eventTypes.Single(t => t.Name == eventName);
+
+        public string GetEventKey<TEvent>()
+        {
+            return typeof(TEvent).Name;
+        }     
+
+        private void RaiseOnEventRemoved(string eventName)
+        {
+            var handler = OnEventRemoved;
+            handler?.Invoke(this, eventName);
+        }
+
+        private SubscriptionInfo? FindSubscriptionToRemove<TEvent, THandler>() where TEvent : IntegrationEvent where THandler : IIntegrationEventHandler<TEvent>
+        {
+            var eventName = GetEventKey<TEvent>();
+            if (!EventHasSubscribers(eventName))
+            {
+                return null;
+            }
+
+            return _handlers[eventName].SingleOrDefault(s => s.HandlerType == typeof(THandler));
+        }
+    }
+}
