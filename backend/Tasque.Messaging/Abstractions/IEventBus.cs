6d8a05eedf9adb2c28ba72f90dfddf37ec1b07b3
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-09-08 22:19:07 +0300

    EventBus was added

---
+++ backend/Tasque.Messaging/Abstractions/IEventBus.cs
@@ -0,0 +1,11 @@
+﻿using Tasque.Core.Common.Models;
+
+namespace Tasque.Messaging.Abstractions
+{
+    public interface IEventBus
+    {
+        void Publish(IntegrationEvent @event);
+        void Subscribe<TEvent, THandler>() where TEvent : IntegrationEvent where THandler : IIntegrationEventHandler<TEvent>;
+        void Unsubscribe<TEvent, THandler>() where TEvent : IntegrationEvent where THandler : IIntegrationEventHandler<TEvent>;
+    }
+}
