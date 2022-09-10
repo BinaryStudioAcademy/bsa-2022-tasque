40448939e6066d289db5ee9713dabc4e41f85c4b
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-09-11 00:57:49 +0300

    Generic handler was added

---
+++ backend/Tasque.Messaging/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasque.Core.Common.Models;
+
+namespace Tasque.Messaging.Abstractions
+{
+    public interface IIntegrationEventHandler<TIntegrationEvent> where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+}
