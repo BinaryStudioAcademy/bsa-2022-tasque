47f21bfc47b09dc1590df400213ec133018636eb
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-09-11 16:00:13 +0300

    InviteUser was added to ProjectService

--- backend/Tasque.BLL/Services/ProjectService.cs
+++ backend/Tasque.BLL/Services/ProjectService.cs
@@ -1,21 +1,30 @@
 ﻿using Tasque.Core.Common.DTO.Project;
 using Tasque.Core.Common.Entities;
+using Tasque.Core.Common.Models.Events;
 using Tasque.Core.DAL;
+using Tasque.Messaging.Abstractions;
 using Task = System.Threading.Tasks.Task;
 
 namespace Tasque.Core.BLL.Services;
 
 public class ProjectService : EntityCrudService<Project>
 {
-    public ProjectService(DataContext db) : base(db)
+    private readonly IEventBus _bus;
+    public ProjectService(DataContext db, IEventBus bus) : base(db)
     {
-
+        _bus = bus;
     }
 
     public async Task<bool> InviteUser(InviteUserDTO dto)
     {
-        var user = _db.Users.SingleOrDefault(u => u.Email == dto.UserEmail || u.IsEmailConfirmed);
-        if (user is null)
+        var invitee = _db.Users.SingleOrDefault(u => u.Email == dto.InviteeEmail || u.IsEmailConfirmed);
+        if (invitee is null)
+        {
+            return false;
+        }
+
+        var invitor = _db.Users.SingleOrDefault(u => u.Email == dto.InvitorEmail || u.IsEmailConfirmed);
+        if (invitor is null)
         {
             return false;
         }
@@ -24,14 +33,21 @@ public class ProjectService : EntityCrudService<Project>
 
         var invitation = new ProjectInvitation
         {
-            User = user,
+            User = invitee,
             Project = project,
         };
 
         _db.Invitations.Add(invitation);
         await _db.SaveChangesAsync();
 
-        // TODO send notification
+        UserInvitedEvent @event = new UserInvitedEvent
+        {
+            ProjectId = project.Id,
+            InviteeId = invitee.Id,
+            InvitorId = invitor.Id
+        };
+
+        _bus.Publish(@event);
 
         return true;
     }
