244cbb393485598420d38031a863d7449fd83a8f
oleksandr-vovk <91162284+oleksandr-vovk@users.noreply.github.com>
2022-09-12 17:37:45 +0300

    avatar fixes (#214)
    * Changed avatar generation colors
    
    * Added tooltip to avatar
    
    * Uppercased initials on avatar

--- frontend/src/shared/components/tasque-avatar/avatar.component.ts
+++ frontend/src/shared/components/tasque-avatar/avatar.component.ts
@@ -20,6 +20,8 @@ export class AvatarComponent implements OnInit {
   userAvatar: string;
 
   fontSize: number;
+  colors = ['#D47500', '#00AA55', '#E3BC01', '#009FD4', '#B281B3', '#D47500', '#DC2929'];
+  public background: string;
 
   constructor() { }
 
@@ -29,6 +31,9 @@ export class AvatarComponent implements OnInit {
       this.userAvatar = '';
     } else {
         this.userAvatar = this.user.avatarURL as string;
+    }
+    if(this.user) {
+      this.background = this.colors[this.user.id % this.colors.length];
     }
   }
 
@@ -40,12 +45,12 @@ export class AvatarComponent implements OnInit {
 
       const partsOfName = user.name.split(' ');
       if (partsOfName.length >= 2) {
-        return partsOfName[0][0] + partsOfName[1][0];
+        return (partsOfName[0][0] + partsOfName[1][0]).toLocaleUpperCase();
       }
 
       return partsOfName[0][0];
     } if(user.name !== undefined) {
-      return user.name.charAt(0);
+      return user.name.charAt(0).toLocaleUpperCase();
     } 
     return '';
   }
