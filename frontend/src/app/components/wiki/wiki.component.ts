329e1283e7da19d5d9cc666c6ec589358a7f5dcf
Daniil <101814817+DaniilPoiarkov@users.noreply.github.com>
2022-09-13 17:05:11 +0300

    Added missing routes (#222)
    * Added missing routes
    
    * Lint

---
+++ frontend/src/app/components/wiki/wiki.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wiki',
+  templateUrl: './wiki.component.html',
+  styleUrls: ['./wiki.component.sass']
+})
+export class WikiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
