55b5d498d9b39999471621f881b36796816839b2
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-09-13 16:47:06 +0300

    deleteSprint

---
+++ frontend/src/app/components/delete-sprint/delete-sprint-dialog/delete-sprint-dialog.component.ts
@@ -0,0 +1,35 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SprintModel } from 'src/core/models/sprint/sprint-model';
+import { NotificationService } from 'src/core/services/notification.service';
+import { SprintService } from 'src/core/services/sprint.service';
+
+@Component({
+  selector: 'app-delete-sprint-dialog',
+  templateUrl: './delete-sprint-dialog.component.html',
+  styleUrls: ['./delete-sprint-dialog.component.sass'],
+})
+export class DeleteSprintDialogComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public sprint: SprintModel,
+    private sprintService: SprintService,
+    private notificationService: NotificationService,
+  ) {}
+
+  ngOnInit(): void {}
+
+  deleteSprint(): void {
+    this.sprintService.delete(this.sprint.id).subscribe(
+      (resp) => {
+        if (resp.ok && resp.body != null) {
+          this.notificationService.success('Sprint was successfully changed');
+        } else {
+          this.notificationService.error('Something went wrong');
+        }
+      },
+      (error) => {
+        this.notificationService.error(error);
+      },
+    );
+  }
+}
--- frontend/src/app/components/delete-sprint/delete-sprint.component.ts
+++ frontend/src/app/components/delete-sprint/delete-sprint.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SprintModel } from 'src/core/models/sprint/sprint-model';
+import { DeleteSprintDialogComponent } from './delete-sprint-dialog/delete-sprint-dialog.component';
 
 @Component({
   selector: 'app-delete-sprint',
   templateUrl: './delete-sprint.component.html',
-  styleUrls: ['./delete-sprint.component.sass']
+  styleUrls: ['./delete-sprint.component.sass'],
 })
 export class DeleteSprintComponent implements OnInit {
+  @Input() public sprint: SprintModel;
 
-  constructor() { }
+  constructor(public matDialog: MatDialog) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  openDialog(): void {
+    const dialog = this.matDialog.open(DeleteSprintDialogComponent, {
+      data: this.sprint,
+    });
+
+    dialog.afterClosed().subscribe((result: SprintModel) => {});
   }
-
 }
