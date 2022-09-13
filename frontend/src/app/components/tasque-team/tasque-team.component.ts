05da7471709da7197f4e79a376cdf55b427f1a02
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-09-13 16:32:14 +0300

    team page now uses real data

--- frontend/src/app/components/tasque-team/tasque-team.component.ts
+++ frontend/src/app/components/tasque-team/tasque-team.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { UserModel } from 'src/core/models/user/user-model';
+import { ProjectService } from 'src/core/services/project.service';
 
 @Component({
   selector: 'app-tasque-team',
@@ -10,112 +12,28 @@ import { UserModel } from 'src/core/models/user/user-model';
 export class TasqueTeamComponent implements OnInit {
   icon = faMagnifyingGlass;
 
-  public loaded: UserModel[] = [
-    {
-      id: 1,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=fox',
-      name: 'Test User',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 2,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=yeen',
-      name: 'Test User 2',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 3,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=snek',
-      name: 'Test User 3',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 4,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=poss',
-      name: 'Test User 4',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 4,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=leo',
-      name: 'Test User 5',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 4,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=serval',
-      name: 'Test User 6',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 4,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=racc',
-      name: 'Test User 7',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 1,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=bear',
-      name: 'Test User 8',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 2,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=caracal',
-      name: 'Test User 9',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 3,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=tig',
-      name: 'Test User 10',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 4,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=puma',
-      name: 'Test User 11',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 4,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=yote',
-      name: 'Test User 12',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 4,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=jaguar',
-      name: 'Test User 13',
-      email: '',
-      organizationRoles: [],
-    },
-    {
-      id: 4,
-      avatarURL: 'https://api.tinyfox.dev/img?animal=capy',
-      name: 'Test User 14',
-      email: '',
-      organizationRoles: [],
-    },
-  ];
+  public loaded: UserModel[];
   public visible: UserModel[];
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private projectService: ProjectService,
+  ) {}
 
   ngOnInit(): void {
-    //TODO: Load team members here
+    const id = this.route.parent?.snapshot.paramMap.get('id');
+    if (!id) {
+      this.router.navigate(['not-found']);
+      return;
+    }
+
+    this.projectService
+      .getProjectParticipants(parseInt(id))
+      .subscribe((res) => {
+        this.loaded = res.body as Array<UserModel>;
+        this.visible = this.loaded;
+      });
     this.visible = this.loaded;
   }
 
