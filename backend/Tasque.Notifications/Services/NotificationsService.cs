178ad4c84ee7d509ae9c629ffad187ef5b34941f
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-09-14 18:13:59 +0300

    Notifications service was added

---
+++ backend/Tasque.Notifications/Services/NotificationsService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tasque.Core.Common.Entities.Notifications;
+using Tasque.Notifications.Data;
+
+namespace Tasque.Notifications.Services
+{
+    public class NotificationsService<TNotification> where TNotification : Notification
+    {
+        private NotificationsContext _db;
+        public NotificationsService(NotificationsContext db)
+        {
+            _db = db;
+        }
+
+        public Task<List<TNotification>> GetNotifications(int recieverId)
+        {
+            return _db.Set<TNotification>().Where(n => n.RecieverId == recieverId).ToListAsync();
+        }
+    }
+}
