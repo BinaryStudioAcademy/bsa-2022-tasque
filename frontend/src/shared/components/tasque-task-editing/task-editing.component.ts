dc6dfdeaba017c39be0da11eb28a30fa22e5f79d
andpew <edarkmail@gmail.com>
2022-09-14 12:18:33 +0300

    Refactored task editing component

--- frontend/src/shared/components/tasque-task-editing/task-editing.component.ts
+++ frontend/src/shared/components/tasque-task-editing/task-editing.component.ts
@@ -1,28 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TaskModel } from 'src/core/models/task/task-model';
 import { UserModel } from 'src/core/models/user/user-model';
-import {
-  faCheckToSlot,
-  faXmark,
-  faLink,
-  faPaperclip,
-  faShareNodes,
-  faEllipsisVertical,
-  faFaceSmile,
-  faPen,
-  faFlag,
-} from '@fortawesome/free-solid-svg-icons';
+import { faCheckToSlot, faXmark, faLink, faPaperclip, faShareNodes, faEllipsisVertical, faFaceSmile, faPen, faFlag } from '@fortawesome/free-solid-svg-icons';
 import { BaseComponent } from 'src/core/base/base.component';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProjectModel } from 'src/core/models/project/project-model';
 import { SprintModel } from 'src/core/models/sprint/sprint-model';
 import { EditorConfig } from 'src/core/settings/angular-editor-setting';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { TaskState } from 'src/core/models/task/task-state';
 import { TaskPriority } from 'src/core/models/task/task-priority';
 import { TaskType } from 'src/core/models/task/task-type';
 import { TasqueDropdownOption } from '../tasque-dropdown/dropdown.component';
+import { GetCurrentUserService } from 'src/core/services/get-current-user.service';
+import { ProjectService } from 'src/core/services/project.service';
+import { TaskTemplateService } from 'src/core/services/task-template.service';
+import { GetCurrentOrganizationService } from 'src/core/services/get-current-organization.service';
+import { SprintService } from 'src/core/services/sprint.service';
+import { TaskService } from 'src/core/services/task.service';
 
 @Component({
   selector: 'tasque-task-editing',
@@ -32,11 +27,10 @@ import { TasqueDropdownOption } from '../tasque-dropdown/dropdown.component';
 export class TaskEditingComponent extends BaseComponent implements OnInit {
   @Input() public task: TaskModel;
   @Input() public currentUser: UserModel;
-  public taskUser: UserModel;
-  public taskReporter: UserModel;
-  public taskSprint: SprintModel;
 
-  public editTaskForm: FormGroup = {} as FormGroup;
+  public organizationId: number;
+
+  public editTaskForm = {} as FormGroup;
 
   public closeIcon = faXmark;
   public linkIcon = faLink;
@@ -57,90 +51,58 @@ export class TaskEditingComponent extends BaseComponent implements OnInit {
   public projectOptions: TasqueDropdownOption[] = [];
   public sprintOptions: TasqueDropdownOption[] = [];
 
-  @Input() public taskPriorities: TaskPriority[] = [];
-  @Input() public taskStates: TaskState[] = [];
-  @Input() public taskTypes: TaskType[] = [];
+  public taskPriorities: TaskPriority[] = [];
+  public taskStates: TaskState[] = [];
+  public taskTypes: TaskType[] = [];
 
-  @Input() public projects: ProjectModel[] = [];
-
-  @Input() public sprints: SprintModel[] = [];
-  @Input() public users: UserModel[] = [];
+  public users: UserModel[] = [];
 
   public editorConfig: AngularEditorConfig = EditorConfig;
 
-  constructor(private sanitizer: DomSanitizer) {
+  // eslint-disable-next-line max-params
+  constructor(
+    private fb: FormBuilder,
+    private currentUserService: GetCurrentUserService,
+    private taskTemplateService: TaskTemplateService,
+    private projectService: ProjectService,
+    private currentOrganizationService: GetCurrentOrganizationService,
+    private taskService: TaskService,
+    private sprintService: SprintService) {
     super();
-
-    this.task = {
-      id: 1,
-      summary: 'Summary',
-      description: 'Description',
-      state: {
-        id: 1,
-        name: 'To Do',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      type: {
-        id: 1,
-        name: 'Bug',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        icon: this.flagIcon,
-      },
-      priority: {
-        id: 1,
-        name: 'Low',
-        type: 0,
-        projectId: 5,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      author: this.users.filter((u) => u.id == 1)[0],
-      project: this.projects.filter((p) => p.id == 2)[0],
-      sprint: this.sprints.filter((s) => s.id == 3)[0],
-      lastUpdatedBy: this.users.filter((u) => u.id == 4)[0],
-      parentTaskId: 5,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      deadline: new Date(),
-      custmFields: [],
-    };
-    this.currentUser = this.users[1];
-    this.taskReporter = this.users[2];
   }
 
   ngOnInit(): void {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    this.taskSprint = this.sprints.find((x) => x.id === this.task.sprint.id)!;
-
-    this.editTaskForm = new FormGroup({
-      taskProject: new FormControl(this.convertToOption(this.task.project)),
-      taskSummary: new FormControl(this.task.summary, [
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(80),
-      ]),
-      taskStatus: new FormControl(this.convertToOption(this.task.state)),
-      taskPriority: new FormControl(this.convertToOption(this.task.priority)),
-      taskType: new FormControl(this.convertToOption(this.task.type)),
-      taskSprint: new FormControl(this.convertToOption(this.taskSprint)),
-      taskDescription: new FormControl<SafeHtml | undefined>(
-        this.task.description,
-        [Validators.maxLength(5000)],
-      ),
-      taskAssignees: new FormControl(),
+    this.currentUserService.currentUser$.subscribe((user) => {
+      this.currentUser = user;
     });
 
-    this.fillProjectOptions(this.projects);
-    this.fillSprintOptions(this.sprints);
-    this.fillTaskStateOptions(this.taskStates);
-    this.fillTaskPriorityOptions(this.taskPriorities);
-    this.fillTaskTypeOptions(this.taskTypes);
-  }
+    this.currentOrganizationService.currentOrganizationId$.subscribe((orgId) => {
+      this.organizationId = orgId;
 
-  public safeHTML(unsafe: string): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(unsafe);
+      this.projectService
+        .getProjectsByOrganizationId(this.organizationId)
+        .subscribe((resp) => {
+          if (!resp.body) {
+            return;
+          }
+          this.fillProjectOptions(resp.body);
+          this.getProjectInfo(this.task.project);
+        });
+    });
+
+    this.editTaskForm = this.fb.group({
+      taskProject: [this.convertToOption(this.task.project)],
+      taskSummary: [this.task.summary,
+      [Validators.minLength(2),
+      Validators.maxLength(80)]
+      ],
+      taskSprint: [this.convertToOption(this.task.sprint)],
+      taskStatus: [this.convertToOption(this.task.state)],
+      taskPriority: [this.convertToOption(this.task.priority)],
+      taskType: [this.convertToOption(this.task.type)],
+      taskDescription: [this.task.description, [Validators.maxLength(5000)]],
+      taskAssignees: [this.task.users],
+    });
   }
 
   summaryClick(): void {
@@ -189,48 +151,47 @@ export class TaskEditingComponent extends BaseComponent implements OnInit {
   }
 
   private convertToOption(
-    item: SprintModel | ProjectModel | TaskState | TaskPriority | TaskType,
-  ): TasqueDropdownOption {
-    if (item.id === 0) {
+    item: TaskState | TaskPriority | TaskType | SprintModel | ProjectModel | undefined): TasqueDropdownOption {
+    if (!item) {
       return {
-        color: 'lightgray',
-        title: '-',
         id: 0,
+        title: '-',
+        color: this.getColorById(0)
       };
     }
 
-    switch (item.id % 4) {
-      case 1:
-        return {
-          color: 'green',
-          title: item.name,
-          id: item.id,
-        };
-      case 2:
-        return {
-          color: 'yellow',
-          title: item.name,
-          id: item.id,
-        };
-      case 3:
-        return {
-          color: 'orange',
-          title: item.name,
-          id: item.id,
-        };
-      case 0:
-        return {
-          color: 'red',
-          title: item.name,
-          id: item.id,
-        };
+    if ('color' in item) {
+      return {
+        id: item.id,
+        title: item.name,
+        color: item.color
+      };
     }
 
     return {
-      color: 'pink',
-      title: 'error',
-      id: -1,
+      id: item.id,
+      title: item.name,
+      color: this.getColorById(item.id)
     };
+  }
+
+  private getColorById(id: number): string {
+    if (id === 0) {
+      return 'lightgray';
+    }
+
+    switch (id % 4) {
+      case 1:
+        return 'green';
+      case 2:
+        return 'yellow';
+      case 3:
+        return 'orange';
+      case 0:
+        return 'red';
+    }
+
+    return 'lightgray';
   }
 
   private fillProjectOptions(projects: ProjectModel[]): void {
@@ -243,9 +204,9 @@ export class TaskEditingComponent extends BaseComponent implements OnInit {
   private fillSprintOptions(sprints: SprintModel[]): void {
     this.sprintOptions = [
       {
-        color: 'lightgray',
         title: '-',
         id: 0,
+        color: this.getColorById(0),
       },
     ];
 
@@ -272,6 +233,36 @@ export class TaskEditingComponent extends BaseComponent implements OnInit {
     this.taskTypeOptions = [];
     taskTypes.forEach((element) => {
       this.taskTypeOptions.push(this.convertToOption(element));
+    });
+  }
+
+  private getProjectInfo(project: ProjectModel): void {
+    this.sprintService.getProjectSprints(project.id).subscribe((sprints) => {
+      if (!sprints.body) {
+        return;
+      }
+      this.fillSprintOptions(sprints.body);
+    });
+
+    this.taskTemplateService.getAllProjectTaskTypes(project.id).subscribe((taskTypes) => {
+      if (!taskTypes.body) {
+        return;
+      }
+      this.fillTaskTypeOptions(taskTypes.body);
+    });
+
+    this.projectService.getProjectPriorities(project.id).subscribe((taskPriorities) => {
+      if (!taskPriorities.body) {
+        return;
+      }
+      this.fillTaskPriorityOptions(taskPriorities.body);
+    });
+
+    this.taskTemplateService.getAllProjectTaskStates(project.id).subscribe((taskStates) => {
+      if (!taskStates.body) {
+        return;
+      }
+      this.fillTaskStateOptions(taskStates.body);
     });
   }
 }
