bc54fa2ca88ba4972285e8de0cb0cda73d80ba61
FeloniukM <felonukmakcim@gmail.com>
2022-09-14 16:08:11 +0300

    fix merge bug

--- backend/Tasque.Core.WebAPI/Controllers/ProjectController.cs
+++ backend/Tasque.Core.WebAPI/Controllers/ProjectController.cs
@@ -22,15 +22,6 @@ public class ProjectController : EntityController
         return Ok(await _service.GetAllProjectsOfOrganization(organizationId));
     }
 
-    [HttpGet("getById/{id}")]
-    public override IActionResult GetById(int id)
-    {
-        var project = _service.GetProjectById(id);
-        if(project == null)
-            return NotFound("Project Not Found");
-        return Ok(project);
-        
-    }
 
     [HttpPut("invite")]
     public async Task<IActionResult> InviteUserToProject([FromBody] UserInviteDto userInviteDto)
