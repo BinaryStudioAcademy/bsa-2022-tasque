d2a3053b2a62925cb93a2746c159ed11925bbb48
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-09-14 20:37:15 +0300

    Properties of DTO were renamed

--- backend/Tasque.Core.Common/DTO/Board/MoveTaskDTO.cs
+++ backend/Tasque.Core.Common/DTO/Board/MoveTaskDTO.cs
@@ -9,7 +9,8 @@ namespace Tasque.Core.Common.DTO.Board
     public class MoveTaskDTO
     {
         public int TaskId { get; set; }
-        public int PreviousBoardId { get; set; }
-        public int CurrentBoardId { get; set; }
+        public int PreviousColumnId { get; set; }
+        public int NewColumnId { get; set; }
+        public int MovedById { get; set; }
     }
 }
