a1ae39114fa2758508165068aa42e14c3769893b
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-09-16 13:56:29 +0300

    updated model

--- frontend/src/core/models/board/task-Info-model.ts
+++ frontend/src/core/models/board/task-Info-model.ts
@@ -6,21 +6,22 @@ import { UserModel } from '../user/user-model';
 import { TaskState } from '../task/task-state';
 
 export interface TaskInfoModel {
-    id: number;
-    type?: TaskType;
-    typeId: number;
-    priority?: TaskPriority;
-    priorityId?: number;
-    state?: TaskState;
-    stateId?: number;
-    summary: string;
-    customLabels: LabelField[];
-    key: string;
-    author?: UserModel;
-    assignees?: UserModel[];
-    attachments: Attachment[],
-    createdAt: Date,
-    updatedAt: Date,
-    deadline: Date,
-    isHidden: boolean;
+  id: number;
+  type?: TaskType;
+  typeId: number;
+  priority?: TaskPriority;
+  priorityId?: number;
+  state?: TaskState;
+  stateId?: number;
+  summary: string;
+  customLabels: LabelField[];
+  key: string;
+  author?: UserModel;
+  assignees?: UserModel[];
+  attachments: Attachment[];
+  createdAt: Date;
+  updatedAt: Date;
+  deadline: Date;
+  isHidden: boolean;
+  order: number;
 }
