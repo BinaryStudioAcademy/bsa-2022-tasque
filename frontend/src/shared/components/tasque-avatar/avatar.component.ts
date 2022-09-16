8ab96cbca94b6cd45a57ad868b56f42e67111a2f
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-09-16 11:06:56 +0300

    added isShowMatTooltip

--- frontend/src/shared/components/tasque-avatar/avatar.component.ts
+++ frontend/src/shared/components/tasque-avatar/avatar.component.ts
@@ -13,44 +13,54 @@ export class AvatarComponent implements OnInit {
   @Input()
   alt: string;
 
+  @Input() isShowMatTooltip = true;
+
   @Input()
   user: UserModel;
 
   userAvatar: string;
   fontSize: number;
 
-  colors = ['#D47500', '#00AA55', '#E3BC01', '#009FD4', '#B281B3', '#D47500', '#DC2929'];
+  colors = [
+    '#D47500',
+    '#00AA55',
+    '#E3BC01',
+    '#009FD4',
+    '#B281B3',
+    '#D47500',
+    '#DC2929',
+  ];
   public background: string;
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
     this.fontSize = this.diameter_px
-    ? this.diameter_px / 2
-    : this.el.nativeElement.offsetWidth / 2.5;
-    if(this.user === undefined || this.user.avatarURL === undefined) {
+      ? this.diameter_px / 2
+      : this.el.nativeElement.offsetWidth / 2.5;
+    if (this.user === undefined || this.user.avatarURL === undefined) {
       this.userAvatar = '';
     } else {
-        this.userAvatar = this.user.avatarURL as string;
+      this.userAvatar = this.user.avatarURL as string;
     }
-    if(this.user) {
+    if (this.user) {
       this.background = this.colors[this.user.id % this.colors.length];
     }
   }
 
   getInitials(user: UserModel): string {
-    if(user === undefined) {
+    if (user === undefined) {
       return '';
     }
-    if(user.name !== undefined && user.name?.includes(' ')) {
-
+    if (user.name !== undefined && user.name?.includes(' ')) {
       const partsOfName = user.name.split(' ');
       if (partsOfName.length >= 2) {
         return (partsOfName[0][0] + partsOfName[1][0]).toLocaleUpperCase();
       }
 
       return partsOfName[0][0];
-    } if(user.name !== undefined) {
+    }
+    if (user.name !== undefined) {
       return user.name.charAt(0).toLocaleUpperCase();
     }
     return '';
