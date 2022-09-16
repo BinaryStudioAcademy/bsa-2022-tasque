e006a7782a089149122e0f0709b2c0fc449d9469
v.gomenyuk@std.npu.edu.ua <v.gomenyuk@std.npu.edu.ua>
2022-09-16 09:11:49 +0300

    removed unnecessary code

--- frontend/src/shared/components/tasque-task-creation/task-creation.component.ts
+++ frontend/src/shared/components/tasque-task-creation/task-creation.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { faEllipsisVertical, faL } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NotificationService } from 'src/core/services/notification.service';
 import { Subject } from 'rxjs';
