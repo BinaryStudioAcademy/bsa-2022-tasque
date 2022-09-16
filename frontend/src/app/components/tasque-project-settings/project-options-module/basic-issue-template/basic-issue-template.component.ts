f144a179f95df7c7d630cbd49859f09a193358e5
andpew <edarkmail@gmail.com>
2022-09-16 09:37:06 +0300

    Added all project info loading

--- frontend/src/app/components/tasque-project-settings/project-options-module/basic-issue-template/basic-issue-template.component.ts
+++ frontend/src/app/components/tasque-project-settings/project-options-module/basic-issue-template/basic-issue-template.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { concatMap, map } from 'rxjs/operators';
+import { concatMap, map, takeUntil } from 'rxjs/operators';
 import { BaseComponent } from 'src/core/base/base.component';
 import { ProjectModel } from 'src/core/models/project/project-model';
+import { TaskPriority } from 'src/core/models/task/task-priority';
+import { TaskState } from 'src/core/models/task/task-state';
+import { TaskType } from 'src/core/models/task/task-type';
 import { GetCurrentProjectService } from 'src/core/services/get-current-project.service';
 import { ProjectService } from 'src/core/services/project.service';
+import { TaskTemplateService } from 'src/core/services/task-template.service';
 import { templateSetting } from './template-setting-model';
 
 @Component({
@@ -15,10 +19,14 @@ export class BasicIssueTemplateComponent extends BaseComponent implements OnInit
   public selectedSetting?: templateSetting;
 
   public project: ProjectModel;
+  public priorities: TaskPriority[];
+  public types: TaskType[];
+  public states: TaskState[];
 
   constructor(
     private getProjectService: GetCurrentProjectService,
-    private projectService: ProjectService) {
+    private projectService: ProjectService,
+    private taskTemplateService: TaskTemplateService) {
     super();
   }
 
@@ -39,10 +47,37 @@ export class BasicIssueTemplateComponent extends BaseComponent implements OnInit
 
     switch (this.selectedSetting) {
       case 'Priority':
+        this.projectService
+          .getProjectPriorities(this.project.id)
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe((priorities) => {
+            if (!priorities.body) {
+              return;
+            }
+            this.priorities = priorities.body;
+          });
         return;
       case 'State':
+        this.taskTemplateService
+          .getAllProjectTaskStates(this.project.id)
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe((states) => {
+            if (!states.body) {
+              return;
+            }
+            this.states = states.body;
+          });
         return;
       case 'Type':
+        this.taskTemplateService
+          .getAllProjectTaskTypes(this.project.id)
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe((types) => {
+            if (!types.body) {
+              return;
+            }
+            this.types = types.body;
+          });
         return;
       case 'Project':
         return;
