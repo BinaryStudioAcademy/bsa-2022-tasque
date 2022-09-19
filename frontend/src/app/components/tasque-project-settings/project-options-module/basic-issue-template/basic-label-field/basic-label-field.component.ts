b1796addaf048b9bf25a70fdb5a334432964bd55
andpew <edarkmail@gmail.com>
2022-09-19 07:52:22 +0300

    Implemented save functionality And finalized project basic settings template page

--- frontend/src/app/components/tasque-project-settings/project-options-module/basic-issue-template/basic-label-field/basic-label-field.component.ts
+++ frontend/src/app/components/tasque-project-settings/project-options-module/basic-issue-template/basic-label-field/basic-label-field.component.ts
@@ -1,18 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { faCheck, faMinus, faPencil, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { takeUntil } from 'rxjs/operators';
+import { BaseComponent } from 'src/core/base/base.component';
 import { ProjectModel } from 'src/core/models/project/project-model';
 import { TaskPriority } from 'src/core/models/task/task-priority';
 import { TaskState } from 'src/core/models/task/task-state';
 import { TaskType } from 'src/core/models/task/task-type';
 import { NotificationService } from 'src/core/services/notification.service';
+import { ProjectService } from 'src/core/services/project.service';
+import { TemplateSetting } from '../template-setting-model';
 
 @Component({
   selector: 'app-basic-label-field',
   templateUrl: './basic-label-field.component.html',
   styleUrls: ['./basic-label-field.component.sass']
 })
-export class BasicLabelFieldComponent implements OnInit {
+export class BasicLabelFieldComponent extends BaseComponent implements OnInit {
   @Input()
   public set settings(v: TaskType[] | TaskState[] | TaskPriority[]) {
     if (!v) {
@@ -40,10 +44,14 @@ export class BasicLabelFieldComponent implements OnInit {
   formColorControl: FormControl;
 
   isChanging = false;
+  isUpdated = false;
 
   constructor(
     private notificatinService: NotificationService,
-  ) { }
+    private projectService: ProjectService
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
     this.formNameControl = new FormControl('', [
@@ -73,6 +81,7 @@ export class BasicLabelFieldComponent implements OnInit {
     for (let index = 0; index < this.settingsShow.length; index++) {
       if (this.settingsShow[index] === setting) {
         this.settingsShow.splice(index, 1);
+        this.isUpdated = true;
         return;
       }
     }
@@ -98,6 +107,7 @@ export class BasicLabelFieldComponent implements OnInit {
 
     this.formColorControl.setValue('');
     this.formNameControl.setValue('');
+    this.isUpdated = true;
   }
 
   public addSetting(): void {
@@ -106,9 +116,55 @@ export class BasicLabelFieldComponent implements OnInit {
 
   public cancelEdit(): void {
     this.settingsShow = Array.from(this.defaultValue);
+    this.isUpdated = false;
   }
 
   public saveEdit(): void {
-    throw new Error('Method not implemented.');
+    if (!this.isUpdated) {
+      this.notificatinService.info('There are no changes. Update values');
+      return;
+    }
+
+    switch (this.type) {
+      case 'Priority':
+        this.projectService.updateProjectTaskPriorities(this.project.id, this.settingsShow as TaskPriority[])
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe((resp) => {
+            if (!resp.body) {
+              return;
+            }
+            this.settingsShow = Array.from(resp.body);
+            this.defaultValue = Array.from(resp.body);
+            this.notificatinService.success('Task Priorities were successfully updated');
+            this.isUpdated = false;
+          });
+        return;
+      case 'State':
+        this.projectService.updateProjectTaskStates(this.project.id, this.settingsShow as TaskState[])
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe((resp) => {
+            if (!resp.body) {
+              return;
+            }
+            this.settingsShow = Array.from(resp.body);
+            this.defaultValue = Array.from(resp.body);
+            this.notificatinService.success('Task States were successfully updated');
+            this.isUpdated = false;
+          });
+        return;
+      case 'Type':
+        this.projectService.updateProjectTaskTypes(this.project.id, this.settingsShow as TaskType[])
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe((resp) => {
+            if (!resp.body) {
+              return;
+            }
+            this.settingsShow = Array.from(resp.body);
+            this.defaultValue = Array.from(resp.body);
+            this.notificatinService.success('Task Types were successfully updated');
+            this.isUpdated = false;
+          });
+        return;
+    }
   }
 }
