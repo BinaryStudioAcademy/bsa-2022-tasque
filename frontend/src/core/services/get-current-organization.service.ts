be9e54756cba3b1a30f71476f626566b84928a09
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-09-19 19:58:17 +0300

    implemented functionality

--- frontend/src/core/services/get-current-organization.service.ts
+++ frontend/src/core/services/get-current-organization.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { UserService } from 'src/app/user/services/user.service';
 import { LocalStorageKeys } from '../models/local-storage-keys';
 import { OrganizationModel } from '../models/organization/organization-model';
+import { GetCurrentUserService } from './get-current-user.service';
 
 @Injectable({ providedIn: 'root' })
 export class GetCurrentOrganizationService {
-  constructor() {}
+  constructor(
+    private userService: UserService,
+    private currentUserService: GetCurrentUserService,
+  ) {
+    const sessionId = sessionStorage.getItem(LocalStorageKeys.selectedOrganization);
+    if (sessionId) return;
+
+    const userId = this.currentUserService.getUserId();
+    this.userService
+      .getLastOrg(userId)
+      .pipe(
+        take(1),
+        map((x) => x.body),
+      )
+      .subscribe((x) => (this.currentOrganizationId = x as number));
+  }
 
   private currentOrganizationIdSubj = new BehaviorSubject<number>(
     this.currentOrganizationId,
@@ -13,16 +31,18 @@ export class GetCurrentOrganizationService {
   public currentOrganizationId$ = this.currentOrganizationIdSubj.asObservable();
 
   public set currentOrganizationId(value: number) {
-    localStorage.setItem(
+    sessionStorage.setItem(
       LocalStorageKeys.selectedOrganization,
       value.toString(),
     );
+    const userId = this.currentUserService.getUserId();
+    this.userService.setLastOrg(userId, value).pipe(take(1)).subscribe();
     this.currentOrganizationIdSubj.next(value);
   }
 
   public get currentOrganizationId(): number {
     return +(
-      localStorage.getItem(LocalStorageKeys.selectedOrganization) ?? '-1'
+      sessionStorage.getItem(LocalStorageKeys.selectedOrganization) ?? '-1'
     );
   }
 
