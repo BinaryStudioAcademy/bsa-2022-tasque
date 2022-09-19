3af817561f88901e53a03328dbd06c0958a2a4ef
andpew <edarkmail@gmail.com>
2022-09-19 04:11:41 +0300

    Updated basic label field template

--- frontend/src/app/components/tasque-project-settings/project-options-module/basic-issue-template/basic-label-field/basic-label-field.component.ts
+++ frontend/src/app/components/tasque-project-settings/project-options-module/basic-issue-template/basic-label-field/basic-label-field.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { faCheck, faMinus, faPencil, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { ProjectModel } from 'src/core/models/project/project-model';
 import { TaskPriority } from 'src/core/models/task/task-priority';
 import { TaskState } from 'src/core/models/task/task-state';
@@ -13,9 +15,42 @@ export class BasicLabelFieldComponent implements OnInit {
   @Input() public settings: TaskType[] | TaskState[] | TaskPriority[];
   @Input() public project: ProjectModel;
 
+  applyIcon = faCheck;
+  removeIcon = faMinus;
+  addIcon = faPlus;
+  editIcon = faPencil;
+
+  formNameControl: FormControl;
+  formColorControl: FormControl;
+
+  isChanging = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public deleteSetting(_setting: TaskType | TaskState | TaskPriority): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public editSetting(_setting: TaskType | TaskState | TaskPriority): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public saveSetting(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public addSetting(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public cancelEdit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public saveEdit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
