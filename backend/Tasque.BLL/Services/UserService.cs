c09a42858760a4fdf19343e6cd799ce049a65ef3
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-09-22 14:49:43 +0300

    Added assignation of connectionId

--- backend/Tasque.BLL/Services/UserService.cs
+++ backend/Tasque.BLL/Services/UserService.cs
@@ -122,5 +122,15 @@ namespace Tasque.Core.BLL.Services
 
             return user.LastOrganizationId;
         }
+
+        public async Task SetConnectionId(int userId, string connectionId)
+        {
+            var user = await _context.Users.FindAsync(userId)
+                ?? throw new CustomNotFoundException("user");
+
+            user.ConnectionId = connectionId;
+
+            await _context.SaveChangesAsync();
+        }
     }
 }
