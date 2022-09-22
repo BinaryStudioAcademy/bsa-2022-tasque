e17570fdaaeb3ede148f15a9f46e4b18be5d191e
Oleh Prostakov <prostakov.oleh@gmail.com>
2022-09-22 21:42:36 +0300

    environments were updated

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://16.170.225.154:80',
-  notificationsUrl: 'http://16.170.225.154:5500/api/'
+  notificationsUrl: 'http://16.170.225.154:5500/notifications/',
+  signalR_hub: 'https://localhost:6001/notifications'
 };
