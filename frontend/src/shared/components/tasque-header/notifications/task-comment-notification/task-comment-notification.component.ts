ed535a1233ab5bc6013c215ba81c8f7934afeee2
unknown <daniilpoyarkov@gmail.com>
2022-09-26 22:08:45 +0300

    Added pipe to transform card labels

--- frontend/src/shared/components/tasque-header/notifications/task-comment-notification/task-comment-notification.component.ts
+++ frontend/src/shared/components/tasque-header/notifications/task-comment-notification/task-comment-notification.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Notification } from 'src/core/models/notifications/notification';
 import { TaskCommentedNotification } from 'src/core/models/notifications/task-commented-notification';
+import { TaskModel } from 'src/core/models/task/task-model';
+import { UserModel } from 'src/core/models/user/user-model';
 
 @Component({
   selector: 'task-comment-notification',
@@ -11,6 +13,25 @@ export class TaskCommentNotificationComponent implements OnInit {
 
   @Input() notification: TaskCommentedNotification;
   @Output() deleteNotification = new EventEmitter<Notification>();
+  commentBy: UserModel = {
+    id: -1,
+    name: 'Daniil',
+    email: 'test@mail.com',
+    organizationRoles: [],
+    avatarURL: 'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80'
+  };
+  task: TaskModel = {
+    id: -1,
+    summary: 'Test',
+    order: 1,
+    attachments: [],
+    typeId: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deadline: new Date(),
+  };
+  comment: string = 'Test';
+
   constructor() { }
 
   ngOnInit(): void {
@@ -18,6 +39,10 @@ export class TaskCommentNotificationComponent implements OnInit {
 
   onDelete(): void {
     this.deleteNotification.emit(this.notification);
+  }
+
+  getTaskRef(): void {
+    console.log(this.task); //TODO: Implement ability to open task-card by link and redirect to it
   }
 
 }
